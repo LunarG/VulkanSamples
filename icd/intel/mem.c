5975c1d3353012f6595e3793a8bd1333700b43f2
Courtney Goeltzenleuchter <courtney@LunarG.com>
2014-08-06 17:09:11 -0600

    intel: Prevent divide by zero when alignment is 0.

--- icd/intel/mem.c
+++ icd/intel/mem.c
@@ -32,7 +32,7 @@ XGL_RESULT intel_mem_alloc(struct intel_dev *dev,
 {
     struct intel_mem *mem;
 
-    if (4096 % info->alignment)
+    if ((info->alignment != 0) && (4096 % info->alignment))
         return XGL_ERROR_INVALID_ALIGNMENT;
     if (info->heapCount != 1 || info->heaps[0] != 0)
         return XGL_ERROR_INVALID_POINTER;
