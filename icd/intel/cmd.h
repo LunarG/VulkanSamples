0914213c739d1907f85d72374d3a0dca8bd592a7
Chia-I Wu <olvaffe@gmail.com>
2014-08-11 15:42:55 +0800

    intel: move cmd stubs to cmd.c
    Most stubs will be moved to cmd_<foo>.c when implemented.

---
+++ icd/intel/cmd.h
@@ -0,0 +1,281 @@
+/*
+ * XGL
+ *
+ * Copyright (C) 2014 LunarG, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+#ifndef CMD_H
+#define CMD_H
+
+#include "intel.h"
+#include "obj.h"
+
+XGL_RESULT XGLAPI intelCreateCommandBuffer(
+    XGL_DEVICE                                  device,
+    const XGL_CMD_BUFFER_CREATE_INFO*           pCreateInfo,
+    XGL_CMD_BUFFER*                             pCmdBuffer);
+
+XGL_RESULT XGLAPI intelBeginCommandBuffer(
+    XGL_CMD_BUFFER                              cmdBuffer,
+    XGL_FLAGS                                   flags);
+
+XGL_RESULT XGLAPI intelEndCommandBuffer(
+    XGL_CMD_BUFFER                              cmdBuffer);
+
+XGL_RESULT XGLAPI intelResetCommandBuffer(
+    XGL_CMD_BUFFER                              cmdBuffer);
+
+XGL_VOID XGLAPI intelCmdBindPipeline(
+    XGL_CMD_BUFFER                              cmdBuffer,
+    XGL_PIPELINE_BIND_POINT                     pipelineBindPoint,
+    XGL_PIPELINE                                pipeline);
+
+XGL_VOID XGLAPI intelCmdBindPipelineDelta(
+    XGL_CMD_BUFFER                              cmdBuffer,
+    XGL_PIPELINE_BIND_POINT                     pipelineBindPoint,
+    XGL_PIPELINE_DELTA                          delta);
+
+XGL_VOID XGLAPI intelCmdBindStateObject(
+    XGL_CMD_BUFFER                              cmdBuffer,
+    XGL_STATE_BIND_POINT                        stateBindPoint,
+    XGL_STATE_OBJECT                            state);
+
+XGL_VOID XGLAPI intelCmdBindDescriptorSet(
+    XGL_CMD_BUFFER                              cmdBuffer,
+    XGL_PIPELINE_BIND_POINT                     pipelineBindPoint,
+    XGL_UINT                                    index,
+    XGL_DESCRIPTOR_SET                          descriptorSet,
+    XGL_UINT                                    slotOffset);
+
+XGL_VOID XGLAPI intelCmdBindDynamicMemoryView(
+    XGL_CMD_BUFFER                              cmdBuffer,
+    XGL_PIPELINE_BIND_POINT                     pipelineBindPoint,
+    const XGL_MEMORY_VIEW_ATTACH_INFO*          pMemView);
+
+XGL_VOID XGLAPI intelCmdBindIndexData(
+    XGL_CMD_BUFFER                              cmdBuffer,
+    XGL_GPU_MEMORY                              mem,
+    XGL_GPU_SIZE                                offset,
+    XGL_INDEX_TYPE                              indexType);
+
+XGL_VOID XGLAPI intelCmdBindAttachments(
+    XGL_CMD_BUFFER                              cmdBuffer,
+    XGL_UINT                                    colorAttachmentCount,
+    const XGL_COLOR_ATTACHMENT_BIND_INFO*       pColorAttachments,
+    const XGL_DEPTH_STENCIL_BIND_INFO*          pDepthStencilAttachment);
+
+XGL_VOID XGLAPI intelCmdPrepareMemoryRegions(
+    XGL_CMD_BUFFER                              cmdBuffer,
+    XGL_UINT                                    transitionCount,
+    const XGL_MEMORY_STATE_TRANSITION*          pStateTransitions);
+
+XGL_VOID XGLAPI intelCmdPrepareImages(
+    XGL_CMD_BUFFER                              cmdBuffer,
+    XGL_UINT                                    transitionCount,
+    const XGL_IMAGE_STATE_TRANSITION*           pStateTransitions);
+
+XGL_VOID XGLAPI intelCmdDraw(
+    XGL_CMD_BUFFER                              cmdBuffer,
+    XGL_UINT                                    firstVertex,
+    XGL_UINT                                    vertexCount,
+    XGL_UINT                                    firstInstance,
+    XGL_UINT                                    instanceCount);
+
+XGL_VOID XGLAPI intelCmdDrawIndexed(
+    XGL_CMD_BUFFER                              cmdBuffer,
+    XGL_UINT                                    firstIndex,
+    XGL_UINT                                    indexCount,
+    XGL_INT                                     vertexOffset,
+    XGL_UINT                                    firstInstance,
+    XGL_UINT                                    instanceCount);
+
+XGL_VOID XGLAPI intelCmdDrawIndirect(
+    XGL_CMD_BUFFER                              cmdBuffer,
+    XGL_GPU_MEMORY                              mem,
+    XGL_GPU_SIZE                                offset,
+    XGL_UINT32                                  count,
+    XGL_UINT32                                  stride);
+
+XGL_VOID XGLAPI intelCmdDrawIndexedIndirect(
+    XGL_CMD_BUFFER                              cmdBuffer,
+    XGL_GPU_MEMORY                              mem,
+    XGL_GPU_SIZE                                offset,
+    XGL_UINT32                                  count,
+    XGL_UINT32                                  stride);
+
+XGL_VOID XGLAPI intelCmdDispatch(
+    XGL_CMD_BUFFER                              cmdBuffer,
+    XGL_UINT                                    x,
+    XGL_UINT                                    y,
+    XGL_UINT                                    z);
+
+XGL_VOID XGLAPI intelCmdDispatchIndirect(
+    XGL_CMD_BUFFER                              cmdBuffer,
+    XGL_GPU_MEMORY                              mem,
+    XGL_GPU_SIZE                                offset);
+
+XGL_VOID XGLAPI intelCmdCopyMemory(
+    XGL_CMD_BUFFER                              cmdBuffer,
+    XGL_GPU_MEMORY                              srcMem,
+    XGL_GPU_MEMORY                              destMem,
+    XGL_UINT                                    regionCount,
+    const XGL_MEMORY_COPY*                      pRegions);
+
+XGL_VOID XGLAPI intelCmdCopyImage(
+    XGL_CMD_BUFFER                              cmdBuffer,
+    XGL_IMAGE                                   srcImage,
+    XGL_IMAGE                                   destImage,
+    XGL_UINT                                    regionCount,
+    const XGL_IMAGE_COPY*                       pRegions);
+
+XGL_VOID XGLAPI intelCmdCopyMemoryToImage(
+    XGL_CMD_BUFFER                              cmdBuffer,
+    XGL_GPU_MEMORY                              srcMem,
+    XGL_IMAGE                                   destImage,
+    XGL_UINT                                    regionCount,
+    const XGL_MEMORY_IMAGE_COPY*                pRegions);
+
+XGL_VOID XGLAPI intelCmdCopyImageToMemory(
+    XGL_CMD_BUFFER                              cmdBuffer,
+    XGL_IMAGE                                   srcImage,
+    XGL_GPU_MEMORY                              destMem,
+    XGL_UINT                                    regionCount,
+    const XGL_MEMORY_IMAGE_COPY*                pRegions);
+
+XGL_VOID XGLAPI intelCmdCloneImageData(
+    XGL_CMD_BUFFER                              cmdBuffer,
+    XGL_IMAGE                                   srcImage,
+    XGL_IMAGE_STATE                             srcImageState,
+    XGL_IMAGE                                   destImage,
+    XGL_IMAGE_STATE                             destImageState);
+
+XGL_VOID XGLAPI intelCmdUpdateMemory(
+    XGL_CMD_BUFFER                              cmdBuffer,
+    XGL_GPU_MEMORY                              destMem,
+    XGL_GPU_SIZE                                destOffset,
+    XGL_GPU_SIZE                                dataSize,
+    const XGL_UINT32*                           pData);
+
+XGL_VOID XGLAPI intelCmdFillMemory(
+    XGL_CMD_BUFFER                              cmdBuffer,
+    XGL_GPU_MEMORY                              destMem,
+    XGL_GPU_SIZE                                destOffset,
+    XGL_GPU_SIZE                                fillSize,
+    XGL_UINT32                                  data);
+
+XGL_VOID XGLAPI intelCmdClearColorImage(
+    XGL_CMD_BUFFER                              cmdBuffer,
+    XGL_IMAGE                                   image,
+    const XGL_FLOAT                             color[4],
+    XGL_UINT                                    rangeCount,
+    const XGL_IMAGE_SUBRESOURCE_RANGE*          pRanges);
+
+XGL_VOID XGLAPI intelCmdClearColorImageRaw(
+    XGL_CMD_BUFFER                              cmdBuffer,
+    XGL_IMAGE                                   image,
+    const XGL_UINT32                            color[4],
+    XGL_UINT                                    rangeCount,
+    const XGL_IMAGE_SUBRESOURCE_RANGE*          pRanges);
+
+XGL_VOID XGLAPI intelCmdClearDepthStencil(
+    XGL_CMD_BUFFER                              cmdBuffer,
+    XGL_IMAGE                                   image,
+    XGL_FLOAT                                   depth,
+    XGL_UINT32                                  stencil,
+    XGL_UINT                                    rangeCount,
+    const XGL_IMAGE_SUBRESOURCE_RANGE*          pRanges);
+
+XGL_VOID XGLAPI intelCmdResolveImage(
+    XGL_CMD_BUFFER                              cmdBuffer,
+    XGL_IMAGE                                   srcImage,
+    XGL_IMAGE                                   destImage,
+    XGL_UINT                                    rectCount,
+    const XGL_IMAGE_RESOLVE*                    pRects);
+
+XGL_VOID XGLAPI intelCmdSetEvent(
+    XGL_CMD_BUFFER                              cmdBuffer,
+    XGL_EVENT                                   event);
+
+XGL_VOID XGLAPI intelCmdResetEvent(
+    XGL_CMD_BUFFER                              cmdBuffer,
+    XGL_EVENT                                   event);
+
+XGL_VOID XGLAPI intelCmdMemoryAtomic(
+    XGL_CMD_BUFFER                              cmdBuffer,
+    XGL_GPU_MEMORY                              destMem,
+    XGL_GPU_SIZE                                destOffset,
+    XGL_UINT64                                  srcData,
+    XGL_ATOMIC_OP                               atomicOp);
+
+XGL_VOID XGLAPI intelCmdBeginQuery(
+    XGL_CMD_BUFFER                              cmdBuffer,
+    XGL_QUERY_POOL                              queryPool,
+    XGL_UINT                                    slot,
+    XGL_FLAGS                                   flags);
+
+XGL_VOID XGLAPI intelCmdEndQuery(
+    XGL_CMD_BUFFER                              cmdBuffer,
+    XGL_QUERY_POOL                              queryPool,
+    XGL_UINT                                    slot);
+
+XGL_VOID XGLAPI intelCmdResetQueryPool(
+    XGL_CMD_BUFFER                              cmdBuffer,
+    XGL_QUERY_POOL                              queryPool,
+    XGL_UINT                                    startQuery,
+    XGL_UINT                                    queryCount);
+
+XGL_VOID XGLAPI intelCmdWriteTimestamp(
+    XGL_CMD_BUFFER                              cmdBuffer,
+    XGL_TIMESTAMP_TYPE                          timestampType,
+    XGL_GPU_MEMORY                              destMem,
+    XGL_GPU_SIZE                                destOffset);
+
+XGL_VOID XGLAPI intelCmdInitAtomicCounters(
+    XGL_CMD_BUFFER                              cmdBuffer,
+    XGL_PIPELINE_BIND_POINT                     pipelineBindPoint,
+    XGL_UINT                                    startCounter,
+    XGL_UINT                                    counterCount,
+    const XGL_UINT32*                           pData);
+
+XGL_VOID XGLAPI intelCmdLoadAtomicCounters(
+    XGL_CMD_BUFFER                              cmdBuffer,
+    XGL_PIPELINE_BIND_POINT                     pipelineBindPoint,
+    XGL_UINT                                    startCounter,
+    XGL_UINT                                    counterCount,
+    XGL_GPU_MEMORY                              srcMem,
+    XGL_GPU_SIZE                                srcOffset);
+
+XGL_VOID XGLAPI intelCmdSaveAtomicCounters(
+    XGL_CMD_BUFFER                              cmdBuffer,
+    XGL_PIPELINE_BIND_POINT                     pipelineBindPoint,
+    XGL_UINT                                    startCounter,
+    XGL_UINT                                    counterCount,
+    XGL_GPU_MEMORY                              destMem,
+    XGL_GPU_SIZE                                destOffset);
+
+XGL_VOID XGLAPI intelCmdDbgMarkerBegin(
+    XGL_CMD_BUFFER                              cmdBuffer,
+    const XGL_CHAR*                             pMarker);
+
+XGL_VOID XGLAPI intelCmdDbgMarkerEnd(
+    XGL_CMD_BUFFER                              cmdBuffer);
+
+#endif /* CMD_H */
