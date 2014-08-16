69abeacf5574a917479bf23d3e9d7b26a574f99e
Chia-I Wu <olvaffe@gmail.com>
2014-08-16 12:43:53 +0800

    icd: add common utility functions

--- icd/common/icd-utils.h
+++ icd/common/icd-utils.h
@@ -22,23 +22,35 @@
  * DEALINGS IN THE SOFTWARE.
  */
 
-#ifndef INTEL_H
-#define INTEL_H
+#ifndef ICD_UTILS_H
+#define ICD_UTILS_H
 
-#include <stdlib.h>
-#include <stdbool.h>
-#include <string.h>
 #include <assert.h>
-
-#include <xgl.h>
-#include <xglDbg.h>
-
 #include "icd.h"
-#include "icd-utils.h"
 
-#define INTEL_API_VERSION XGL_MAKE_VERSION(0, 22, 0)
-#define INTEL_DRIVER_VERSION 0
+#define ARRAY_SIZE(a) (sizeof(a) / sizeof(a[0]))
 
-#define INTEL_GEN(gen) ((int) ((gen) * 100))
+#define STATIC_ASSERT(expr) do {            \
+    (void) sizeof(char[1 - 2 * !(expr)]);   \
+} while (0)
 
-#endif /* INTEL_H */
+/**
+ * Return true if val is power of two, or zero.
+ */
+static inline bool u_is_pow2(unsigned int val)
+{
+    return ((val & (val - 1)) == 0);
+}
+
+static inline unsigned int u_align(unsigned int val, unsigned alignment)
+{
+    assert(alignment && u_is_pow2(alignment));
+    return (val + alignment - 1) & ~(alignment - 1);
+}
+
+static inline unsigned int u_minify(unsigned int val, unsigned level)
+{
+    return (val >> level) ? val >> level : 1;
+}
+
+#endif /* ICD_UTILS_H */
