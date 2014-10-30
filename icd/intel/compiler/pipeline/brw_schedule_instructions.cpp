0d5f951b3443eac20d24fbe95a26fea0db25c054
Cody Northrop <cody@lunarg.com>
2014-10-30 10:19:47 -0600

    Add LunarG copyright to files with substantative changes

--- icd/intel/compiler/pipeline/brw_schedule_instructions.cpp
+++ icd/intel/compiler/pipeline/brw_schedule_instructions.cpp
@@ -1,5 +1,6 @@
 /*
  * Copyright © 2010 Intel Corporation
+ * Copyright (C) 2014 LunarG, Inc.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
