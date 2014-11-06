ca21a21bf712837a8f15c2421a18338cb0ce7e01
Courtney Goeltzenleuchter <courtney@LunarG.com>
2014-11-06 14:27:52 -0700

    demo: cube: Process destroy window
    Demo was not exiting cleanly when the window close button
    was clicked.
    Found this solution on-line:
    http://lists.freedesktop.org/pipermail/xcb/2010-December/006713.html

--- demos/cube.c
+++ demos/cube.c
@@ -260,6 +260,7 @@ struct demo {
     XGL_CMD_BUFFER cmd;
 
     xcb_window_t window;
+    xcb_intern_atom_reply_t *atom_wm_delete_window;
 
     bool quit;
     XGL_UINT current_buffer;
@@ -1136,9 +1137,16 @@ static void demo_prepare(struct demo *demo)
 static void demo_handle_event(struct demo *demo,
                               const xcb_generic_event_t *event)
 {
-    switch (event->response_type & 0x7f) {
+    u_int8_t event_code = event->response_type & 0x7f;
+    switch (event_code) {
     case XCB_EXPOSE:
         demo_draw(demo);
+        break;
+    case XCB_CLIENT_MESSAGE:
+        if((*(xcb_client_message_event_t*)event).data.data32[0] ==
+           (*demo->atom_wm_delete_window).atom) {
+            demo->quit = true;
+        }
         break;
     case XCB_KEY_RELEASE:
         {
@@ -1208,6 +1216,19 @@ static void demo_create_window(struct demo *demo)
             XCB_WINDOW_CLASS_INPUT_OUTPUT,
             demo->screen->root_visual,
             value_mask, value_list);
+
+    /* Magic code that will send notification when window is destroyed */
+    xcb_intern_atom_cookie_t cookie = xcb_intern_atom(demo->connection, 1, 12,
+                                                      "WM_PROTOCOLS");
+    xcb_intern_atom_reply_t* reply = xcb_intern_atom_reply(demo->connection, cookie, 0);
+
+    xcb_intern_atom_cookie_t cookie2 = xcb_intern_atom(demo->connection, 0, 16, "WM_DELETE_WINDOW");
+    demo->atom_wm_delete_window = xcb_intern_atom_reply(demo->connection, cookie2, 0);
+
+    xcb_change_property(demo->connection, XCB_PROP_MODE_REPLACE,
+                        demo->window, (*reply).atom, 4, 32, 1,
+                        &(*demo->atom_wm_delete_window).atom);
+    free(reply);
 
     xcb_map_window(demo->connection, demo->window);
 }
