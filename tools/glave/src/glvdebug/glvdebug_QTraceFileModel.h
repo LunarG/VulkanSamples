f7933d72321c48e51962f8c08a43bf8e04ab47dd
Peter Lohrmann <peterl@valvesoftware.com>
2014-12-10 16:02:00 -0800

    Reorganize the ModelIndex creation in QTraceFileModel to better follow Qt's intended approach. The call to index(..) now creates the ModelIndex and stores a pointer to the data that is associated with that specific index (row + column + parent combination) and the call to data(..) simply interprets that data. This allows for better searching and other advanced functionality that we'll want in the future. QTraceFileModel also now has support for a Duration column (which is also displayed in the API call tree) which shows the CPU duration of the time it took to create the packet (which should closely reflect the duration that the API call took to execute when it was traced).

--- tools/glave/src/glvdebug/glvdebug_QTraceFileModel.h
+++ tools/glave/src/glvdebug/glvdebug_QTraceFileModel.h
@@ -22,18 +22,41 @@ public:
 
     int rowCount(const QModelIndex &parent = QModelIndex()) const
     {
+        if (parent.column() > 0)
+        {
+            return 0;
+        }
+
         int rowCount = 0;
         if (m_pTraceFileInfo != NULL)
         {
             rowCount = m_pTraceFileInfo->packetCount;
         }
 
+        if (parent.isValid())
+        {
+            // there is a valid parent, so this is a child node, which has no rows
+            rowCount = 0;
+        }
+
         return rowCount;
     }
 
+    enum Columns
+    {
+        Column_EntrypointName,
+        Column_TracerId,
+        Column_PacketIndex,
+        Column_BeginTime,
+        Column_EndTime,
+        Column_PacketSize,
+        Column_CpuDuration,
+        cNumColumns
+    };
+
     int columnCount(const QModelIndex &parent = QModelIndex()) const
     {
-        return 6;
+        return cNumColumns;
     }
 
     QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const
@@ -45,47 +68,42 @@ public:
 
         if (role == Qt::DisplayRole)
         {
-            if (index.row() >= 0)
+            switch (index.column())
             {
-                glv_trace_packet_header* pHeader = m_pTraceFileInfo->pPacketOffsets[index.row()].pHeader;
-                switch (index.column())
+                case Column_EntrypointName:
                 {
-                case 0:
+                    glv_trace_packet_header* pHeader = (glv_trace_packet_header*)index.internalPointer();
+                    uint64_t packet_id = pHeader->packet_id;
+                    switch (packet_id)
                     {
-                        uint64_t packet_id = pHeader->packet_id;
-                        switch (packet_id)
+                        case GLV_TPI_MESSAGE:
                         {
-                            case GLV_TPI_MESSAGE:
-                            {
-                                glv_trace_packet_message* msgPacket = (glv_trace_packet_message*)pHeader->pBody;
-                                return QString(msgPacket->message);
-                                break;
-                            }
-                            case GLV_TPI_MARKER_CHECKPOINT:
-                            case GLV_TPI_MARKER_API_BOUNDARY:
-                            case GLV_TPI_MARKER_API_GROUP_BEGIN:
-                            case GLV_TPI_MARKER_API_GROUP_END:
-                            case GLV_TPI_MARKER_TERMINATE_PROCESS:
-                            default:
-                            return QVariant((unsigned int) packet_id);
+                            glv_trace_packet_message* msgPacket = (glv_trace_packet_message*)pHeader->pBody;
+                            return QString(msgPacket->message);
+                            break;
                         }
+                        case GLV_TPI_MARKER_CHECKPOINT:
+                        case GLV_TPI_MARKER_API_BOUNDARY:
+                        case GLV_TPI_MARKER_API_GROUP_BEGIN:
+                        case GLV_TPI_MARKER_API_GROUP_END:
+                        case GLV_TPI_MARKER_TERMINATE_PROCESS:
+                        default:
+                        return QVariant((unsigned int) packet_id);
                     }
-                case 1:
-                    return QVariant(m_pTraceFileInfo->pPacketOffsets[index.row()].pHeader->tracer_id);
-                case 2:
-                    return QVariant((unsigned long long) m_pTraceFileInfo->pPacketOffsets[index.row()].pHeader->global_packet_index);
-                case 3:
-                    return QVariant((unsigned long long) m_pTraceFileInfo->pPacketOffsets[index.row()].pHeader->entrypoint_begin_time);
-                case 4:
-                    return QVariant((unsigned long long) m_pTraceFileInfo->pPacketOffsets[index.row()].pHeader->entrypoint_end_time);
-                case 5:
-                    return QVariant((unsigned long long) m_pTraceFileInfo->pPacketOffsets[index.row()].pHeader->size);
                 }
-            }
-            else
-            {
-                return QString("Row%1, Column%2").arg(index.row() + 1)
-                                                 .arg(index.column() + 1);
+                case Column_TracerId:
+                    return QVariant(*(uint8_t*)index.internalPointer());
+                case Column_PacketIndex:
+                case Column_BeginTime:
+                case Column_EndTime:
+                case Column_PacketSize:
+                    return QVariant(*(unsigned long long*)index.internalPointer());
+                case Column_CpuDuration:
+                {
+                    glv_trace_packet_header* pHeader = (glv_trace_packet_header*)index.internalPointer();
+                    uint64_t duration = pHeader->entrypoint_end_time - pHeader->entrypoint_begin_time;
+                    return QVariant((unsigned int)duration);
+                }
             }
         }
         return QVariant();
@@ -93,7 +111,34 @@ public:
 
     QModelIndex index(int row, int column, const QModelIndex& parent = QModelIndex()) const
     {
-        return createIndex(row, column);
+        glv_trace_packet_header* pHeader = m_pTraceFileInfo->pPacketOffsets[row].pHeader;
+        void* pData = NULL;
+        switch (column)
+        {
+        case Column_EntrypointName:
+            pData = pHeader;
+            break;
+        case Column_TracerId:
+            pData = &pHeader->tracer_id;
+            break;
+        case Column_PacketIndex:
+            pData = &pHeader->global_packet_index;
+            break;
+        case Column_BeginTime:
+            pData = &pHeader->entrypoint_begin_time;
+            break;
+        case Column_EndTime:
+            pData = &pHeader->entrypoint_end_time;
+            break;
+        case Column_PacketSize:
+            pData = &pHeader->size;
+            break;
+        case Column_CpuDuration:
+            pData = pHeader;
+            break;
+        }
+
+        return createIndex(row, column, pData);
     }
 
     QModelIndex parent(const QModelIndex& index) const
@@ -109,18 +154,20 @@ public:
             {
                 switch (section)
                 {
-                case 0:
+                case Column_EntrypointName:
                     return QString("API Call");
-                case 1:
+                case Column_TracerId:
                     return QString("Tracer ID");
-                case 2:
+                case Column_PacketIndex:
                     return QString("Global Packet Index");
-                case 3:
+                case Column_BeginTime:
                     return QString("Start Time");
-                case 4:
+                case Column_EndTime:
                     return QString("End Time");
-                case 5:
+                case Column_PacketSize:
                     return QString("Size (bytes)");
+                case Column_CpuDuration:
+                    return QString("Duration");
                 }
             }
         }
