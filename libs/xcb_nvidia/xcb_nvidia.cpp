29321dd8f83bc74467bfb9aef54540a5007536cc
Ian Elliott <ian@LunarG.com>
2015-03-02 17:49:02 -0700

    Fix "xcb_nvidia.c" for use of the nulldrv.
    We're using "XGL_DRIVERS_PATH" for the env var.

--- libs/xcb_nvidia/xcb_nvidia.cpp
+++ libs/xcb_nvidia/xcb_nvidia.cpp
@@ -30,7 +30,7 @@ xcb_connection_t * xcb_connect(const char *displayname, int *screenp)
     xglNvidia += "\\XGL_nvidia.dll";
     HMODULE module = LoadLibrary(xglNvidia.c_str());
     if (!module) {
-        std::string xglNulldrv = (getenv("LIBXGL_DRIVERS_PATH") == NULL) ? "" : getenv("LIBXGL_DRIVERS_PATH");
+        std::string xglNulldrv = (getenv("XGL_DRIVERS_PATH") == NULL) ? "" : getenv("XGL_DRIVERS_PATH");
         xglNulldrv += "\\xgl_nulldrv.dll";
         module = LoadLibrary(xglNulldrv.c_str());
     }
