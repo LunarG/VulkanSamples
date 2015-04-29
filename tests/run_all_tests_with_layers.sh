8a21cf42767902a15bd9d14e1539f13444b5e2b4
Tobin Ehlis <tobin@lunarg.com>
2015-04-29 10:37:00 -0600

    tests: Update layer test script to enable Validation layer and skip the vk_layer_validation_tests for now as they re-insert layers and cause a hang

--- tests/run_all_tests_with_layers.sh
+++ tests/run_all_tests_with_layers.sh
@@ -3,7 +3,7 @@
 # Run all the regression tests with validation layers enabled
 
 # enable layers
-export LIBVK_LAYER_NAMES=DrawState:MemTracker:ParamChecker:ObjectTracker:Threading
+export LIBVK_LAYER_NAMES=Validation
 # Save any existing settings file
 RESTORE_SETTINGS="false"
 SETTINGS_NAME="vk_layer_settings.txt"
@@ -22,6 +22,7 @@ echo "DrawStateReportLevel = $OUTPUT_LEVEL" >> $SETTINGS_NAME
 echo "ObjectTrackerReportLevel = $OUTPUT_LEVEL" >> $SETTINGS_NAME
 echo "ParamCheckerReportLevel = $OUTPUT_LEVEL" >> $SETTINGS_NAME
 echo "ThreadingReportLevel = $OUTPUT_LEVEL" >> $SETTINGS_NAME
+echo "ShaderCheckerReportLevel = $OUTPUT_LEVEL" >> $SETTINGS_NAME
 
 # vkbase tests that basic VK calls are working (don't return an error).
 ./vkbase
@@ -36,8 +37,6 @@ echo "ThreadingReportLevel = $OUTPUT_LEVEL" >> $SETTINGS_NAME
 # --compare-images will cause the test to check the resulting image against
 # a saved "golden" image and will report an error if there is any difference
 ./vk_render_tests --compare-images
-
-./vk_layer_validation_tests
 
 if [ "$RESTORE_SETTINGS" = "true" ]; then
     echo Restore $SETTINGS_NAME from $TMP_SETTINGS_NAME
