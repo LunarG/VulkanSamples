be11ca167a084e347c1532de4680f9bde60ae171
Jon Ashburn <jon@lunarg.com>
2015-05-05 16:11:15 -0600

    layers: Fix bug of GetGlobalExtensionInfo not being added to layer_intercept

--- vk-layer-generate.py
+++ vk-layer-generate.py
@@ -308,6 +308,7 @@ class Subcommand(object):
                         intercept = self._gen_layer_dbg_callback_unregister()
                     elif 'GetGlobalExtensionInfo' == proto.name:
                         funcs.append(self._gen_layer_get_global_extension_info(self.layer_name))
+                        intercepted.append(proto)
                 if intercept is not None:
                     funcs.append(intercept)
                     intercepted.append(proto)
