37367e6f7b499916c8668cbbdc5232952994ca86
Chris Forbes <chrisf@ijw.co.nz>
2015-05-25 11:13:29 +1200

    tests: Add test for a VS input not provided by a VI attrib
    Signed-off-by: Chris Forbes <chrisf@ijw.co.nz>
    Reviewed-by: Tobin Ehlis <tobin@lunarg.com>

--- tests/layer_validation_tests.cpp
+++ tests/layer_validation_tests.cpp
@@ -1955,6 +1955,55 @@ TEST_F(VkLayerTest, CreatePipelineAttribNotConsumed)
     }
 }
 
+TEST_F(VkLayerTest, CreatePipelineAttribNotProvided)
+{
+    VK_DBG_MSG_TYPE msgType;
+    std::string msgString;
+    ASSERT_NO_FATAL_FAILURE(InitState());
+    ScopedUseSpv spv(true);
+
+    char const *vsSource =
+        "#version 140\n"
+        "#extension GL_ARB_separate_shader_objects: require\n"
+        "#extension GL_ARB_shading_language_420pack: require\n"
+        "\n"
+        "layout(location=0) in vec4 x;\n"       /* not provided */
+        "void main(){\n"
+        "   gl_Position = x;\n"
+        "}\n";
+    char const *fsSource =
+        "#version 140\n"
+        "#extension GL_ARB_separate_shader_objects: require\n"
+        "#extension GL_ARB_shading_language_420pack: require\n"
+        "\n"
+        "layout(location=0) out vec4 color;\n"
+        "void main(){\n"
+        "   color = vec4(1);\n"
+        "}\n";
+
+    VkShaderObj vs(m_device, vsSource, VK_SHADER_STAGE_VERTEX, this);
+    VkShaderObj fs(m_device, fsSource, VK_SHADER_STAGE_FRAGMENT, this);
+
+    VkPipelineObj pipe(m_device);
+    pipe.AddShader(&vs);
+    pipe.AddShader(&fs);
+
+    VkCommandBufferObj dummyCmd(m_device);
+    VkDescriptorSetObj descriptorSet(m_device);
+    descriptorSet.AppendDummy();
+    descriptorSet.CreateVKDescriptorSet(&dummyCmd);
+
+    m_errorMonitor->ClearState();
+    pipe.CreateVKPipeline(descriptorSet);
+
+    msgType = m_errorMonitor->GetState(&msgString);
+
+    ASSERT_EQ(VK_DBG_MSG_ERROR, msgType);
+    if (!strstr(msgString.c_str(),"VS consumes input at location 0 but not provided")) {
+        FAIL() << "Incorrect warning: " << msgString;
+    }
+}
+
 int main(int argc, char **argv) {
     int result;
 
