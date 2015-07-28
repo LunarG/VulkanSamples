c50dbb882d5882ef40124b26f7ce370f001d3582
Tony Barbour <tony@LunarG.com>
2015-07-28 14:00:43 -0600

    tests: Turn on more layer validation layers

--- tests/run_all_tests_with_layers.ps1
+++ tests/run_all_tests_with_layers.ps1
@@ -10,17 +10,26 @@ echo $dPath
 
 Set-Item -path env:Path -value ($env:Path + ";..\loader\$dPath")
 Set-Item -path env:Path -value ($env:Path + ";gtest-1.7.0\$dPath")
-$env:VK_LAYER_NAMES = "Validation"
-$env:VK_LAYERS_PATH = "..\layers\$dPath"
-
+$env:VK_INSTANCE_LAYERS = "Threading:MemTracker:ObjectTracker:DrawState:ParamChecker:ShaderChecker"
+$env:VK_DEVICE_LAYERS = "Threading:MemTracker:ObjectTracker:DrawState:ParamChecker:ShaderChecker"
+$env:VK_LAYER_DIRS = "..\layers\$dPath"
+OUTPUT_FLAGS="error"
+OUTPUT_ACTION="VK_DBG_LAYER_ACTION_LOG_MSG"
 $SETTINGS_NAME="vk_layer_settings.txt"
-$OUTPUT_LEVEL="VK_DBG_LAYER_LEVEL_ERROR"
 
-echo "MemTrackerReportLevel = $OUTPUT_LEVEL" > $SETTINGS_NAME
-echo "DrawStateReportLevel = $OUTPUT_LEVEL" >> $SETTINGS_NAME
-echo "ObjectTrackerReportLevel = $OUTPUT_LEVEL" >> $SETTINGS_NAME
-echo "ParamCheckerReportLevel = $OUTPUT_LEVEL" >> $SETTINGS_NAME
-echo "ThreadingReportLevel = $OUTPUT_LEVEL" >> $SETTINGS_NAME
+# Write out settings file to run tests with
+echo "MemTrackerReportFlags = $OUTPUT_FLAGS" > $SETTINGS_NAME
+echo "MemTrackerDebugAction = $OUTPUT_ACTION" >> $SETTINGS_NAME
+echo "DrawStateReportFlags = $OUTPUT_FLAGS" >> $SETTINGS_NAME
+echo "DrawStateDebugAction = $OUTPUT_ACTION" >> $SETTINGS_NAME
+echo "ObjectTrackerReportFlags = $OUTPUT_FLAGS" >> $SETTINGS_NAME
+echo "ObjectTrackerDebugAction = $OUTPUT_ACTION" >> $SETTINGS_NAME
+echo "ParamCheckerReportFlags = $OUTPUT_FLAGS" >> $SETTINGS_NAME
+echo "ParamCheckerDebugAction = $OUTPUT_ACTION" >> $SETTINGS_NAME
+echo "ThreadingReportFlags = $OUTPUT_FLAGS" >> $SETTINGS_NAME
+echo "ThreadingDebugAction = $OUTPUT_ACTION" >> $SETTINGS_NAME
+echo "ShaderCheckerReportFlags = $OUTPUT_FLAGS" >> $SETTINGS_NAME
+echo "ShaderCheckerDebugAction = $OUTPUT_ACTION" >> $SETTINGS_NAME
 
 & $dPath\vkbase.exe
 & $dPath\vk_blit_tests
--- tests/run_all_tests_with_layers.sh
+++ tests/run_all_tests_with_layers.sh
@@ -3,12 +3,12 @@
 # Run all the regression tests with validation layers enabled
 
 # enable layers
-export LIBVK_LAYER_NAMES=Validation
+export VK_INSTANCE_LAYERS=Threading:MemTracker:ObjectTracker:DrawState:ParamChecker:ShaderChecker
+export VK_DEVICE_LAYERS=Threading:MemTracker:ObjectTracker:DrawState:ParamChecker:ShaderChecker
 # Save any existing settings file
 RESTORE_SETTINGS="false"
 SETTINGS_NAME="vk_layer_settings.txt"
 TMP_SETTINGS_NAME="xls.txt"
-OUTPUT_LEVEL="VK_DBG_LAYER_LEVEL_ERROR"
 OUTPUT_FLAGS="error"
 OUTPUT_ACTION="VK_DBG_LAYER_ACTION_LOG_MSG"
 if [ -f $SETTINGS_NAME ]; then
@@ -21,11 +21,16 @@ fi
 # Write out settings file to run tests with
 echo "MemTrackerReportFlags = $OUTPUT_FLAGS" > $SETTINGS_NAME
 echo "MemTrackerDebugAction = $OUTPUT_ACTION" >> $SETTINGS_NAME
-echo "DrawStateReportLevel = $OUTPUT_LEVEL" >> $SETTINGS_NAME
-echo "ObjectTrackerReportLevel = $OUTPUT_LEVEL" >> $SETTINGS_NAME
-echo "ParamCheckerReportLevel = $OUTPUT_LEVEL" >> $SETTINGS_NAME
-echo "ThreadingReportLevel = $OUTPUT_LEVEL" >> $SETTINGS_NAME
-echo "ShaderCheckerReportLevel = $OUTPUT_LEVEL" >> $SETTINGS_NAME
+echo "DrawStateReportFlags = $OUTPUT_FLAGS" >> $SETTINGS_NAME
+echo "DrawStateDebugAction = $OUTPUT_ACTION" >> $SETTINGS_NAME
+echo "ObjectTrackerReportFlags = $OUTPUT_FLAGS" >> $SETTINGS_NAME
+echo "ObjectTrackerDebugAction = $OUTPUT_ACTION" >> $SETTINGS_NAME
+echo "ParamCheckerReportFlags = $OUTPUT_FLAGS" >> $SETTINGS_NAME
+echo "ParamCheckerDebugAction = $OUTPUT_ACTION" >> $SETTINGS_NAME
+echo "ThreadingReportFlags = $OUTPUT_FLAGS" >> $SETTINGS_NAME
+echo "ThreadingDebugAction = $OUTPUT_ACTION" >> $SETTINGS_NAME
+echo "ShaderCheckerReportFlags = $OUTPUT_FLAGS" >> $SETTINGS_NAME
+echo "ShaderCheckerDebugAction = $OUTPUT_ACTION" >> $SETTINGS_NAME
 
 # vkbase tests that basic VK calls are working (don't return an error).
 ./vkbase
@@ -39,7 +44,7 @@ echo "ShaderCheckerReportLevel = $OUTPUT_LEVEL" >> $SETTINGS_NAME
 #vk_render_tests tests a variety of features using rendered images
 # --compare-images will cause the test to check the resulting image against
 # a saved "golden" image and will report an error if there is any difference
-./vk_render_tests --compare-images
+./vk_render_tests --compare-images 
 
 # vkglavetracereplay.sh tests glave trace and replay
 D=`dirname \`pwd\``
