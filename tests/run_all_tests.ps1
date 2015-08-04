867361ee3ad7c00bd532ce5cd18c7cddd24fa751
Tony Barbour <tony@LunarG.com>
2015-08-04 11:37:59 -0600

    tests: Don't compare images on Windows to goldens

--- tests/run_all_tests.ps1
+++ tests/run_all_tests.ps1
@@ -15,6 +15,6 @@ $env:VK_LAYERS_PATH = "..\layers\$dPath"
 & $dPath\vkbase.exe
 & $dPath\vk_blit_tests
 & $dPath\vk_image_tests
-& $dPath\vk_render_tests --compare-images
+& $dPath\vk_render_tests
 & $dPath\vk_layer_validation_tests
 .\vkglavetracereplay.ps1 "-$dPath"
