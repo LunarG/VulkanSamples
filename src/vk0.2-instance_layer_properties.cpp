a713371d734eab1a7875d4d73c75ca18b51bbae0
Courtney Goeltzenleuchter <courtney@LunarG.com>
2015-08-07 11:42:43 -0600

    init: Add GetGlobalLayerProperties sample

---
+++ src/vk0.2-instance_layer_properties.cpp
@@ -0,0 +1,55 @@
+/*
+ * Vulkan Samples Kit
+ *
+ * Copyright (C) 2015 LunarG, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+/*
+VULKAN_SAMPLE_SHORT_DESCRIPTION
+create and destroy Vulkan instance
+*/
+
+#include <util_init.hpp>
+#include <cstdlib>
+
+int main(int argc, char **argv)
+{
+    VkResult err;
+    struct sample_info info;
+
+/* VULKAN_KEY_START */
+
+    err = init_global_layer_properties(info);
+
+    std::cout << "Instance Layers:" << std::endl;
+    for (std::vector<layer_properties>::iterator it = info.instance_layer_properties.begin();
+         it != info.instance_layer_properties.end();
+         it++) {
+        std::cout << (*it).properties.layerName << " ";
+    }
+
+    std::cout << std::endl;
+
+/* VULKAN_KEY_END */
+
+    return 0;
+}
+
