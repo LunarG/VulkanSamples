fb78927ec02ee81e59a0ebe405288c271fd03886
Tony Barbour <tony@LunarG.com>
2015-08-14 13:27:29 -0600

    Add ppm file for texture and script for running samples

---
+++ src/run_all_samples.sh
@@ -0,0 +1,18 @@
+echo "Depth Buffer"
+./vk0.2-depthbuffer
+echo "Device"
+./vk0.2-device
+echo "Enumerate"
+./vk0.2-enumerate
+echo "Init Texture"
+./vk0.2-initTexture
+echo "Init WSI"
+./vk0.2-initwsi
+echo "Instance"
+./vk0.2-instance
+echo "Instance Layer Properties"
+./vk0.2-instance_layer_properties
+echo "Use GLSL Shader"
+./vk0.2-useglslshader
+echo "Use SPIR-V Shader"
+./vk0.2-usespirvshader
