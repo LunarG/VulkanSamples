5c28898b1d0380c5cd45813671b4a4b05f5c2735
Tony Barbour <tony@LunarG.com>
2015-08-26 14:41:21 -0600

    Update run_all scripts with new samples

--- src/run_all_samples.ps1
+++ src/run_all_samples.ps1
@@ -8,22 +8,32 @@ if ($Debug) {
     $dPath = "Release"
 }
 
-echo "Depth Buffer"
-& $dPath\vk0.2-depthbuffer
-echo "Device"
-& $dPath\vk0.2-device
-echo "Enumerate"
-& $dPath\vk0.2-enumerate
-echo "Init Texture"
-& $dPath\vk0.2-initTexture
-echo "Init WSI"
-& $dPath\vk0.2-initwsi
 echo "Instance"
 & $dPath\vk0.2-instance
 echo "Instance Layer Properties"
 & $dPath\vk0.2-instance_layer_properties
+echo "Enumerate"
+& $dPath\vk0.2-enumerate
+echo "Device"
+& $dPath\vk0.2-device
+echo "Init WSI"
+& $dPath\vk0.2-initwsi
+echo "Init Command Buffer"
+& $dPath\vk0.2-initcommandbuffer
+echo "Depth Buffer"
+& $dPath\vk0.2-depthbuffer
+echo "Init Texture"
+& $dPath\vk0.2-initTexture
 echo "Uniform Buffer"
 & $dPath\vk0.2-uniformbuffer
+echo "Init RenderPass"
+& $dPath\vk0.2-initrenderpass
+echo "Init Framebuffer"
+& $dPath\vk0.2-initframebuffers
+echo "Descriptor and Pipeline Layouts"
+& $dPath\vk0.2-descriptor_pipeline_layouts
+echo "Vertex Buffer"
+& $dPath\vk0.2-vertexbuffer
 echo "Use GLSL Shader"
 & $dPath\vk0.2-useglslshader
 echo "Use SPIR-V Shader"
