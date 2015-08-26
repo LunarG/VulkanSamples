77169657a91966ea3cc57acfef21da0e0eb95173
Tony Barbour <tony@LunarG.com>
2015-08-26 14:41:21 -0600

    Update run_all scripts with new samples

--- src/run_all_samples.sh
+++ src/run_all_samples.sh
@@ -1,19 +1,30 @@
-echo "Depth Buffer"
-./vk0.2-depthbuffer
-echo "Device"
-./vk0.2-device
-echo "Enumerate"
-./vk0.2-enumerate
-echo "Init Texture"
-./vk0.2-initTexture
-echo "Init WSI"
-./vk0.2-initwsi
+
 echo "Instance"
 ./vk0.2-instance
 echo "Instance Layer Properties"
 ./vk0.2-instance_layer_properties
+echo "Enumerate"
+./vk0.2-enumerate
+echo "Device"
+./vk0.2-device
+echo "Init WSI"
+./vk0.2-initwsi
+echo "Init Command Buffer"
+./vk0.2-initcommandbuffer
+echo "Depth Buffer"
+./vk0.2-depthbuffer
+echo "Init Texture"
+./vk0.2-initTexture
 echo "Uniform Buffer"
 ./vk0.2-uniformbuffer
+echo "Init RenderPass"
+./vk0.2-initrenderpass
+echo "Init Framebuffers"
+./vk0.2-initframebuffers
+echo "Descriptor and Pipeline Layouts"
+./vk0.2-descriptor_pipeline_layouts
+echo "Vertex Buffer"
+./vk0.2-vertexbuffer
 echo "Use GLSL Shader"
 ./vk0.2-useglslshader
 echo "Use SPIR-V Shader"
