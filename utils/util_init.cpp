4bf21ac490ee82646110456578e6b7c81cb25aaa
Tony Barbour <tony@LunarG.com>
2015-08-26 11:49:53 -0600

    Add depth buffer code as a utility

--- utils/util_init.cpp
+++ utils/util_init.cpp
@@ -328,6 +328,95 @@ void init_window(struct sample_info &info)
                          XCB_CONFIG_WINDOW_X | XCB_CONFIG_WINDOW_Y, coords);
 }
 #endif // _WIN32
+
+void init_depth_buffer(struct sample_info &info)
+{
+    VkResult res;
+    VkImageCreateInfo image_info = {};
+    const VkFormat depth_format = VK_FORMAT_D16_UNORM;
+    VkFormatProperties props;
+    res = vkGetPhysicalDeviceFormatProperties(info.gpu, depth_format, &props);
+    assert(!res);
+    if (props.linearTilingFeatures & VK_FORMAT_FEATURE_DEPTH_STENCIL_ATTACHMENT_BIT) {
+        image_info.tiling = VK_IMAGE_TILING_LINEAR;
+    } else if (props.optimalTilingFeatures & VK_FORMAT_FEATURE_DEPTH_STENCIL_ATTACHMENT_BIT) {
+        image_info.tiling = VK_IMAGE_TILING_OPTIMAL;
+    } else {
+        /* Try other depth formats? */
+        std::cout << "VK_FORMAT_D16_UNORM Unsupported.\n";
+        exit(-1);
+    }
+
+    image_info.sType = VK_STRUCTURE_TYPE_IMAGE_CREATE_INFO;
+    image_info.pNext = NULL;
+    image_info.imageType = VK_IMAGE_TYPE_2D;
+    image_info.format = depth_format;
+    image_info.extent.width = info.width;
+    image_info.extent.height = info.height;
+    image_info.extent.depth = 1;
+    image_info.mipLevels = 1;
+    image_info.arraySize = 1;
+    image_info.samples = 1;
+    image_info.usage = VK_IMAGE_USAGE_DEPTH_STENCIL_BIT;
+    image_info.flags = 0;
+
+    VkMemoryAllocInfo mem_alloc = {};
+    mem_alloc.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOC_INFO;
+    mem_alloc.pNext = NULL;
+    mem_alloc.allocationSize = 0;
+    mem_alloc.memoryTypeIndex = 0;
+
+    VkAttachmentViewCreateInfo view_info = {};
+    view_info.sType = VK_STRUCTURE_TYPE_ATTACHMENT_VIEW_CREATE_INFO;
+    view_info.pNext = NULL;
+    view_info.image.handle = VK_NULL_HANDLE;
+    view_info.format = depth_format;
+    view_info.mipLevel = 0;
+    view_info.baseArraySlice = 0;
+    view_info.arraySize = 1;
+    view_info.flags = 0;
+
+    VkMemoryRequirements mem_reqs;
+
+    info.depth.format = depth_format;
+
+    /* Create image */
+    res = vkCreateImage(info.device, &image_info,
+                        &info.depth.image);
+    assert(!res);
+
+    res = vkGetImageMemoryRequirements(info.device,
+                                       info.depth.image, &mem_reqs);
+
+    mem_alloc.allocationSize = mem_reqs.size;
+    /* Use the memory properties to determine the type of memory required */
+    res = memory_type_from_properties(info,
+                                      mem_reqs.memoryTypeBits,
+                                      VK_MEMORY_PROPERTY_DEVICE_ONLY,
+                                      &mem_alloc.memoryTypeIndex);
+    assert(!res);
+
+    /* Allocate memory */
+    res = vkAllocMemory(info.device, &mem_alloc, &info.depth.mem);
+    assert(!res);
+
+    /* Bind memory */
+    res = vkBindImageMemory(info.device, info.depth.image,
+                            info.depth.mem, 0);
+    assert(!res);
+
+    /* Set the image layout to depth stencil optimal */
+    set_image_layout(info, info.depth.image,
+                          VK_IMAGE_ASPECT_DEPTH,
+                          VK_IMAGE_LAYOUT_UNDEFINED,
+                          VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL);
+
+    /* Create image view */
+    view_info.image = info.depth.image;
+    res = vkCreateAttachmentView(info.device, &view_info, &info.depth.view);
+    assert(!res);
+}
+
 void init_descriptor_and_pipeline_layouts(struct sample_info &info)
 {
     VkDescriptorSetLayoutBinding layout_binding = {};
