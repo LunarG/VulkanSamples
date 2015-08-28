714631676003b9157132d88b2222019f3fd1e246
Tony Barbour <tony@LunarG.com>
2015-08-28 11:13:54 -0600

    Add glslang to be able to convert glsl to SPIR-V

--- utils/util.cpp
+++ utils/util.cpp
@@ -31,6 +31,7 @@ samples utility functions
 #include <assert.h>
 #include <cstdlib>
 #include "util.hpp"
+#include "SPIRV/GlslangToSpv.h"
 
 using namespace std;
 
@@ -193,5 +194,182 @@ bool read_ppm(char const*const filename, int *width, int *height, uint64_t rowPi
         dataPtr += rowPitch;
     }
     fclose(fPtr);
+    return true;
+}
+
+void init_resources(TBuiltInResource &Resources)
+{
+    Resources.maxLights = 32;
+    Resources.maxClipPlanes = 6;
+    Resources.maxTextureUnits = 32;
+    Resources.maxTextureCoords = 32;
+    Resources.maxVertexAttribs = 64;
+    Resources.maxVertexUniformComponents = 4096;
+    Resources.maxVaryingFloats = 64;
+    Resources.maxVertexTextureImageUnits = 32;
+    Resources.maxCombinedTextureImageUnits = 80;
+    Resources.maxTextureImageUnits = 32;
+    Resources.maxFragmentUniformComponents = 4096;
+    Resources.maxDrawBuffers = 32;
+    Resources.maxVertexUniformVectors = 128;
+    Resources.maxVaryingVectors = 8;
+    Resources.maxFragmentUniformVectors = 16;
+    Resources.maxVertexOutputVectors = 16;
+    Resources.maxFragmentInputVectors = 15;
+    Resources.minProgramTexelOffset = -8;
+    Resources.maxProgramTexelOffset = 7;
+    Resources.maxClipDistances = 8;
+    Resources.maxComputeWorkGroupCountX = 65535;
+    Resources.maxComputeWorkGroupCountY = 65535;
+    Resources.maxComputeWorkGroupCountZ = 65535;
+    Resources.maxComputeWorkGroupSizeX = 1024;
+    Resources.maxComputeWorkGroupSizeY = 1024;
+    Resources.maxComputeWorkGroupSizeZ = 64;
+    Resources.maxComputeUniformComponents = 1024;
+    Resources.maxComputeTextureImageUnits = 16;
+    Resources.maxComputeImageUniforms = 8;
+    Resources.maxComputeAtomicCounters = 8;
+    Resources.maxComputeAtomicCounterBuffers = 1;
+    Resources.maxVaryingComponents = 60;
+    Resources.maxVertexOutputComponents = 64;
+    Resources.maxGeometryInputComponents = 64;
+    Resources.maxGeometryOutputComponents = 128;
+    Resources.maxFragmentInputComponents = 128;
+    Resources.maxImageUnits = 8;
+    Resources.maxCombinedImageUnitsAndFragmentOutputs = 8;
+    Resources.maxCombinedShaderOutputResources = 8;
+    Resources.maxImageSamples = 0;
+    Resources.maxVertexImageUniforms = 0;
+    Resources.maxTessControlImageUniforms = 0;
+    Resources.maxTessEvaluationImageUniforms = 0;
+    Resources.maxGeometryImageUniforms = 0;
+    Resources.maxFragmentImageUniforms = 8;
+    Resources.maxCombinedImageUniforms = 8;
+    Resources.maxGeometryTextureImageUnits = 16;
+    Resources.maxGeometryOutputVertices = 256;
+    Resources.maxGeometryTotalOutputComponents = 1024;
+    Resources.maxGeometryUniformComponents = 1024;
+    Resources.maxGeometryVaryingComponents = 64;
+    Resources.maxTessControlInputComponents = 128;
+    Resources.maxTessControlOutputComponents = 128;
+    Resources.maxTessControlTextureImageUnits = 16;
+    Resources.maxTessControlUniformComponents = 1024;
+    Resources.maxTessControlTotalOutputComponents = 4096;
+    Resources.maxTessEvaluationInputComponents = 128;
+    Resources.maxTessEvaluationOutputComponents = 128;
+    Resources.maxTessEvaluationTextureImageUnits = 16;
+    Resources.maxTessEvaluationUniformComponents = 1024;
+    Resources.maxTessPatchComponents = 120;
+    Resources.maxPatchVertices = 32;
+    Resources.maxTessGenLevel = 64;
+    Resources.maxViewports = 16;
+    Resources.maxVertexAtomicCounters = 0;
+    Resources.maxTessControlAtomicCounters = 0;
+    Resources.maxTessEvaluationAtomicCounters = 0;
+    Resources.maxGeometryAtomicCounters = 0;
+    Resources.maxFragmentAtomicCounters = 8;
+    Resources.maxCombinedAtomicCounters = 8;
+    Resources.maxAtomicCounterBindings = 1;
+    Resources.maxVertexAtomicCounterBuffers = 0;
+    Resources.maxTessControlAtomicCounterBuffers = 0;
+    Resources.maxTessEvaluationAtomicCounterBuffers = 0;
+    Resources.maxGeometryAtomicCounterBuffers = 0;
+    Resources.maxFragmentAtomicCounterBuffers = 1;
+    Resources.maxCombinedAtomicCounterBuffers = 1;
+    Resources.maxAtomicCounterBufferSize = 16384;
+    Resources.maxTransformFeedbackBuffers = 4;
+    Resources.maxTransformFeedbackInterleavedComponents = 64;
+    Resources.maxCullDistances = 8;
+    Resources.maxCombinedClipAndCullDistances = 8;
+    Resources.maxSamples = 4;
+    Resources.limits.nonInductiveForLoops = 1;
+    Resources.limits.whileLoops = 1;
+    Resources.limits.doWhileLoops = 1;
+    Resources.limits.generalUniformIndexing = 1;
+    Resources.limits.generalAttributeMatrixVectorIndexing = 1;
+    Resources.limits.generalVaryingIndexing = 1;
+    Resources.limits.generalSamplerIndexing = 1;
+    Resources.limits.generalVariableIndexing = 1;
+    Resources.limits.generalConstantMatrixVectorIndexing = 1;
+
+}
+
+EShLanguage FindLanguage(const VkShaderStage shader_type)
+{
+    switch (shader_type) {
+    case VK_SHADER_STAGE_VERTEX:
+        return EShLangVertex;
+
+    case VK_SHADER_STAGE_TESS_CONTROL:
+        return EShLangTessControl;
+
+    case VK_SHADER_STAGE_TESS_EVALUATION:
+        return EShLangTessEvaluation;
+
+    case VK_SHADER_STAGE_GEOMETRY:
+        return EShLangGeometry;
+
+    case VK_SHADER_STAGE_FRAGMENT:
+        return EShLangFragment;
+
+    case VK_SHADER_STAGE_COMPUTE:
+        return EShLangCompute;
+
+    default:
+        return EShLangVertex;
+    }
+}
+
+void init_glslang()
+{
+    glslang::InitializeProcess();
+}
+
+void finalize_glslang()
+{
+    glslang::FinalizeProcess();
+}
+
+//
+// Compile a given string containing GLSL into SPV for use by VK
+// Return value of false means an error was encountered.
+//
+bool GLSLtoSPV(const VkShaderStage shader_type,
+               const char *pshader,
+               std::vector<unsigned int> &spirv)
+{
+    glslang::TProgram& program = *new glslang::TProgram;
+    const char *shaderStrings[1];
+    TBuiltInResource Resources;
+    init_resources(Resources);
+
+    EShMessages messages = EShMsgDefault;
+
+    EShLanguage stage = FindLanguage(shader_type);
+    glslang::TShader* shader = new glslang::TShader(stage);
+
+    shaderStrings[0] = pshader;
+    shader->setStrings(shaderStrings, 1);
+
+    if (! shader->parse(&Resources, 100, false, messages)) {
+        puts(shader->getInfoLog());
+        puts(shader->getInfoDebugLog());
+        return false; // something didn't work
+    }
+
+    program.addShader(shader);
+
+    //
+    // Program-level processing...
+    //
+
+    if (! program.link(messages)) {
+        puts(shader->getInfoLog());
+        puts(shader->getInfoDebugLog());
+        return false;
+    }
+
+    glslang::GlslangToSpv(*program.getIntermediate(stage), spirv);
+
     return true;
 }
