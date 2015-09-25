e30cc838c5c773458a7ed197cb14bdef609c66bd
Lisa Owen <lisa@lunarg.com>
2015-09-25 15:51:21 -0600

    mod script to get short descript of all or specified sample

--- src/get-short-descripts.sh
+++ src/get-short-descripts.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
+# return short description of identified or all samples
+#    get-short-descript.sh [sample_source_file_name]
+
 # save command directory
 CMDDIR="$(dirname "$(readlink -f ${BASH_SOURCE[0]})")"
 
-# read all source .cpp files and display the short description
-for f in $CMDDIR/*.cpp
+# determine if a specific sample is requested; if not display all
+SAMP2DISP="$CMDDIR/*.cpp"
+if [[ $# > 0 ]]; then
+    SAMP2DISP=$1
+fi
+
+# read all identified .cpp file(s) and display the short description
+for f in $SAMP2DISP
 do
    SHORT_DESCRIPT=`sed -n '/VULKAN_SAMPLE_SHORT_DESCRIPTION/{n;p}' $f`
    BNAME=`basename $f`
