5fc56a77a70420a25025d99d0c4156726b583f5c
Lisa Owen <lisa@lunarg.com>
2015-09-25 18:44:24 -0600

    enhance get short desc script to omit filename, take single file
    - script now takes --nofname option to suppress display of filename
    - default behaviour is to display descripts for all filenames; can
      now specific "--sampfname filename.cpp" to display short description
      for only that file

--- src/get-short-descripts.sh
+++ src/get-short-descripts.sh
@@ -1,22 +1,47 @@
 #!/bin/bash
 
 # return short description of identified or all samples
-#    get-short-descript.sh [sample_source_file_name]
+#    get-short-descript.sh [--nofname] [--sampfname sample_source_file_name]
+#
+# - uses VULKAN_SAMPLE_SHORT_DESCRIPTION keyword from source file
+# - default (no options) will display short descriptions for all samples
+# - default display format: displays sample file names and short descriptions
+#   filename.cpp:  short descript
+# - specify --nofname to suppress display of the filename
+
+NOFNAME=""
 
 # save command directory
 CMDDIR="$(dirname "$(readlink -f ${BASH_SOURCE[0]})")"
 
-# determine if a specific sample is requested; if not display all
+# default to displaying short descriptions of all samples
 SAMP2DISP="$CMDDIR/*.cpp"
-if [[ $# > 0 ]]; then
-    SAMP2DISP=$1
-fi
+
+
+# parse the arguments
+while test $# -gt 0; do
+        case "$1" in
+        --nofname)
+                NOFNAME=true
+                ;;
+        --sampfname)
+                shift
+                SAMP2DISP=$1
+                ;;
+        esac
+
+        shift
+done
 
 # read all identified .cpp file(s) and display the short description
 for f in $SAMP2DISP
 do
    SHORT_DESCRIPT=`sed -n '/VULKAN_SAMPLE_SHORT_DESCRIPTION/{n;p}' $f`
    BNAME=`basename $f`
-   echo "$BNAME: $SHORT_DESCRIPT"
+   if test -z $NOFNAME; then
+      echo "$BNAME: $SHORT_DESCRIPT"
+   else
+      echo "$SHORT_DESCRIPT"
+   fi
 done
 
