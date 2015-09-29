b6ee713fa630c3a4cf7a0e5b93551abfc39e2c46
Lisa Owen <lisa@lunarg.com>
2015-09-29 17:42:21 -0600

    more general script to run all samples
    - uses ls results to obtain list of samples to run
    - uses get-short-descripts.sh to get description of the sample

--- src/run_all_samples.sh
+++ src/run_all_samples.sh
@@ -1,51 +1,29 @@
+#!/bin/bash
 
-echo "Instance"
-./vk0.9-instance
-echo "Instance Layer Properties"
-./vk0.9-instance_layer_properties
-echo "Instance Extension Properties"
-./vk0.9-instance_extension_properties
-echo "Instance Layer ExtensionProperties"
-./vk0.9-instance_layer_extension_properties
-echo "Create Debug Message Callback"
-./vk0.9-dbgcreatemsgcallback
-echo "Enumerate"
-./vk0.9-enumerate
-echo "Device"
-./vk0.9-device
-echo "Enable Validation and Debug Message Callback"
-./vk0.9-enable_validation_with_callback
-echo "Init WSI"
-./vk0.9-initwsi
-echo "Init Command Buffer"
-./vk0.9-initcommandbuffer
-echo "Depth Buffer"
-./vk0.9-depthbuffer
-echo "Init Texture"
-./vk0.9-inittexture
-echo "Uniform Buffer"
-./vk0.9-uniformbuffer
-echo "Init RenderPass"
-./vk0.9-initrenderpass
-echo "Init Framebuffers"
-./vk0.9-initframebuffers
-echo "Descriptor and Pipeline Layouts"
-./vk0.9-descriptor_pipeline_layouts
-echo "Vertex Buffer"
-./vk0.9-vertexbuffer
-echo "Alloc Descriptor Sets"
-./vk0.9-allocdescriptorsets
-echo "Initialize Shaders"
-./vk0.9-initshaders
-echo "Initialize Pipeline"
-./vk0.9-initpipeline
-echo "Draw Cube"
-./vk0.9-drawcube
-echo "Draw Textured Cube"
-./vk0.9-drawtexturedcube
-echo "Draw Cube With Utilities"
-./vk0.9-drawcubewithutilities
-echo "Use GLSL Shader"
-./vk0.9-useglslshader
-echo "Use SPIR-V Shader"
-./vk0.9-usespirvshader
+# run all of the samples
+
+# save command directory; note that this file is a link to file of same
+# name in samples/src/
+CMDDIR="$(dirname "$(readlink -f ${BASH_SOURCE[0]})")"
+
+# get the list of built samples to run
+SAMP2RUN=`ls $CMDDIR/vk*`
+#echo "SAMP2RUN is $SAMP2RUN"
+
+# display short description of the sample and run it
+for f in $SAMP2RUN
+do
+   # get short description of the sample source file
+   DESCRIPT=`$CMDDIR/get-short-descripts.sh --nofname --sampfname $f`
+   BNAME=$(basename $f)
+   echo "RUNNING SAMPLE:  $BNAME"
+   echo "  ** $DESCRIPT"
+
+   # run the built sample; need to add build to path and remove .cpp from name
+   SNAME=$(echo $f | sed -e "s/src/build\/src/g")
+   #RNAME=$(echo $SNAME | sed -e "s/.cpp//g")
+   RNAME=${SNAME%.cpp}
+   $RNAME
+   echo ""
+done
+
