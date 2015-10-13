353ccb34d977c1a6753cba606482d412520fb3b8
Tony Barbour <tony@LunarG.com>
2015-10-13 14:36:37 -0600

    Fix windows compiler warnings

--- src/vk0.9-initpipeline.cpp
+++ src/vk0.9-initpipeline.cpp
@@ -239,7 +239,7 @@ int main(int argc, char **argv)
         vkDestroyImageView(info.device, info.buffers[i].view);
     }
     info.fpDestroySwapchainKHR(info.device, info.swap_chain);
-    for (int i = 0; i < info.swapchainImageCount; i++) {
+    for (uint32_t i = 0; i < info.swapchainImageCount; i++) {
         vkDestroyFramebuffer(info.device, info.framebuffers[i]);
     }
     free(info.framebuffers);
