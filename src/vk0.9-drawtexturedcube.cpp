d13ef9c15d071576db20892e7ff2d996b12820a6
Cody Northrop <cody@lunarg.com>
2015-10-21 16:33:01 -0600

    port: drawtexturedcube now uses two descriptor sets

--- src/vk0.9-drawtexturedcube.cpp
+++ src/vk0.9-drawtexturedcube.cpp
@@ -42,31 +42,39 @@ const char *vertShaderText =
         "#version 140\n"
         "#extension GL_ARB_separate_shader_objects : enable\n"
         "#extension GL_ARB_shading_language_420pack : enable\n"
-        "layout (binding = 0) uniform buf {\n"
-        "        mat4 mvp;\n"
-        "} ubuf;\n"
+        "layout (set = 0, binding = 0) uniform bufferVals {\n"
+        "    mat4 mvp;\n"
+        "} myBufferVals;\n"
+        "layout (set = 1, binding = 0) uniform sampler2D surface;\n"
         "layout (location = 0) in vec4 pos;\n"
         "layout (location = 1) in vec2 inTexCoords;\n"
-        "layout (location = 0) out vec2 texcoord;\n"
+        "layout (location = 0) out vec4 outColor;\n"
+        "layout (location = 1) out vec2 outTexCoords;\n"
         "void main() {\n"
-        "   texcoord = inTexCoords;\n"
-        "   gl_Position = ubuf.mvp * pos;\n"
-        "\n"
+        "   outColor = texture(surface, vec2(0.0));\n"
+        "   outTexCoords = inTexCoords;\n"
+        "   gl_Position = myBufferVals.mvp * pos;\n"
         "   // GL->VK conventions\n"
         "   gl_Position.y = -gl_Position.y;\n"
         "   gl_Position.z = (gl_Position.z + gl_Position.w) / 2.0;\n"
         "}\n";
 
 const char *fragShaderText=
-    "#version 140\n"
-    "#extension GL_ARB_separate_shader_objects : enable\n"
-    "#extension GL_ARB_shading_language_420pack : enable\n"
-    "layout (binding = 1) uniform sampler2D tex;\n"
-    "layout (location = 0) in vec2 texcoord;\n"
-    "layout (location = 0) out vec4 outColor;\n"
-    "void main() {\n"
-    "   outColor = textureLod(tex, texcoord, 0.0);\n"
-    "}\n";
+        "#version 140\n"
+        "#extension GL_ARB_separate_shader_objects : enable\n"
+        "#extension GL_ARB_shading_language_420pack : enable\n"
+        "layout (location = 0) in vec4 inColor;\n"
+        "layout (location = 1) in vec2 inTexCoords;\n"
+        "layout (location = 0) out vec4 outColor;\n"
+        "void main() {\n"
+        "    vec4 resColor = inColor;\n"
+        // Create a border to see the cube more easily
+        "   if (inTexCoords.x < 0.01 || inTexCoords.x > 0.99)\n"
+        "       resColor *= vec4(0.1, 0.1, 0.1, 1.0);\n"
+        "   if (inTexCoords.y < 0.01 || inTexCoords.y > 0.99)\n"
+        "       resColor *= vec4(0.1, 0.1, 0.1, 1.0);\n"
+        "   outColor = resColor;\n"
+        "}\n";
 
 int main(int argc, char **argv)
 {
@@ -90,21 +98,125 @@ int main(int argc, char **argv)
     init_device_queue(info);
     init_swap_chain(info);
     init_depth_buffer(info);
-    init_texture(info);
+    const char* textureName = "green.ppm";
+    init_texture(info, textureName);
     init_uniform_buffer(info);
-    init_descriptor_and_pipeline_layouts(info, true);
     init_renderpass(info);
     init_shaders(info, vertShaderText, fragShaderText);
     init_framebuffers(info);
     init_vertex_buffer(info, g_vb_texture_Data, sizeof(g_vb_texture_Data),
                        sizeof(g_vb_texture_Data[0]), true);
-    init_descriptor_pool(info, true);
-    init_descriptor_set(info, true);
-    init_pipeline_cache(info);
-    init_pipeline(info);
 
     /* VULKAN_KEY_START */
 
+    // Set up two descriptor sets
+    static const unsigned descriptor_set_count = 2;
+
+    // Create first layout to contain uniform buffer data
+    VkDescriptorSetLayoutBinding uniform_binding[1] = {};
+    uniform_binding[0].descriptorType = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
+    uniform_binding[0].arraySize = 1;
+    uniform_binding[0].stageFlags = VK_SHADER_STAGE_VERTEX_BIT;
+    uniform_binding[0].pImmutableSamplers = NULL;
+    VkDescriptorSetLayoutCreateInfo uniform_layout_info[1] = {};
+    uniform_layout_info[0].sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_LAYOUT_CREATE_INFO;
+    uniform_layout_info[0].pNext = NULL;
+    uniform_layout_info[0].count = 1;
+    uniform_layout_info[0].pBinding = uniform_binding;
+
+    // Create second layout containing combined sampler/image data
+    VkDescriptorSetLayoutBinding sampler2D_binding[1] = {};
+    sampler2D_binding[0].descriptorType = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER;
+    sampler2D_binding[0].arraySize = 1;
+    sampler2D_binding[0].stageFlags = VK_SHADER_STAGE_VERTEX_BIT;
+    sampler2D_binding[0].pImmutableSamplers = NULL;
+    VkDescriptorSetLayoutCreateInfo sampler2D_layout_info[1] = {};
+    sampler2D_layout_info[0].sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_LAYOUT_CREATE_INFO;
+    sampler2D_layout_info[0].pNext = NULL;
+    sampler2D_layout_info[0].count = 1;
+    sampler2D_layout_info[0].pBinding = sampler2D_binding;
+
+    // Create multiple sets, using each createInfo
+    static const unsigned uniform_set_index = 0;
+    static const unsigned sampler_set_index = 1;
+    VkDescriptorSetLayout descriptor_layouts[descriptor_set_count] = {};
+    res = vkCreateDescriptorSetLayout(info.device, uniform_layout_info, &descriptor_layouts[uniform_set_index]);
+    assert(res == VK_SUCCESS);
+    res = vkCreateDescriptorSetLayout(info.device, sampler2D_layout_info, &descriptor_layouts[sampler_set_index]);
+    assert(res == VK_SUCCESS);
+
+    // Create pipeline layout with multiple descriptor sets
+    VkPipelineLayoutCreateInfo pipelineLayoutCreateInfo[1] = {};
+    pipelineLayoutCreateInfo[0].sType                  = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
+    pipelineLayoutCreateInfo[0].pNext                  = NULL;
+    pipelineLayoutCreateInfo[0].pushConstantRangeCount = 0;
+    pipelineLayoutCreateInfo[0].pPushConstantRanges    = NULL;
+    pipelineLayoutCreateInfo[0].descriptorSetCount     = descriptor_set_count;
+    pipelineLayoutCreateInfo[0].pSetLayouts            = descriptor_layouts;
+    res = vkCreatePipelineLayout(info.device, pipelineLayoutCreateInfo, &info.pipeline_layout);
+    assert(res == VK_SUCCESS);
+
+    // Create a single pool to contain data for our two descriptor sets
+    VkDescriptorTypeCount type_count[2] = {};
+    type_count[0].type = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
+    type_count[0].count = 1;
+    type_count[1].type = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER;
+    type_count[1].count = 1;
+
+    VkDescriptorPoolCreateInfo pool_info[1] = {};
+    pool_info[0].sType = VK_STRUCTURE_TYPE_DESCRIPTOR_POOL_CREATE_INFO;
+    pool_info[0].pNext = NULL;
+    pool_info[0].poolUsage = VK_DESCRIPTOR_POOL_USAGE_ONE_SHOT;
+    pool_info[0].maxSets = descriptor_set_count;
+    pool_info[0].count = sizeof(type_count) / sizeof(VkDescriptorTypeCount);
+    pool_info[0].pTypeCount = type_count;
+
+    VkDescriptorPool descriptor_pool[1] = {};
+    res = vkCreateDescriptorPool(info.device, pool_info, descriptor_pool);
+    assert(res == VK_SUCCESS);
+
+    // Populate the descriptor sets
+    VkDescriptorSet descriptor_sets[descriptor_set_count] = {};
+    res = vkAllocDescriptorSets(
+            info.device,
+            descriptor_pool[0],
+            VK_DESCRIPTOR_SET_USAGE_STATIC,
+            descriptor_set_count,
+            descriptor_layouts,
+            descriptor_sets);
+    assert(res == VK_SUCCESS);
+
+    VkWriteDescriptorSet descriptor_writes[2] = {};
+
+    // Populate with info about our uniform buffer
+    descriptor_writes[0].sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET;
+    descriptor_writes[0].pNext = NULL;
+    descriptor_writes[0].destSet = descriptor_sets[uniform_set_index];
+    descriptor_writes[0].count = 1;
+    descriptor_writes[0].descriptorType = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
+    descriptor_writes[0].pDescriptors = &info.uniform_data.desc; // populated by init_uniform_buffer()
+    descriptor_writes[0].destArrayElement = 0;
+    descriptor_writes[0].destBinding = 0;
+
+    // Populate with info about our texture
+    descriptor_writes[1].sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET;
+    descriptor_writes[1].pNext = NULL;
+    descriptor_writes[1].destSet = descriptor_sets[sampler_set_index];
+    descriptor_writes[1].count = 1;
+    descriptor_writes[1].descriptorType = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER;
+    descriptor_writes[1].pDescriptors = &info.texture_data.desc; // populated by init_texture()
+    descriptor_writes[1].destArrayElement = 0;
+    descriptor_writes[1].destBinding = 0;
+
+    vkUpdateDescriptorSets(info.device, descriptor_set_count, descriptor_writes, 0, NULL);
+
+    /* VULKAN_KEY_END */
+
+    // Call remaining boilerplate utils
+    init_pipeline_cache(info);
+    init_pipeline(info);
+
+    // The remaining is identical to drawtexturedcube
     VkClearValue clear_values[2];
     clear_values[0].color.float32[0] = 0.2f;
     clear_values[0].color.float32[1] = 0.2f;
@@ -150,7 +262,7 @@ int main(int argc, char **argv)
     vkCmdBindPipeline(info.cmd, VK_PIPELINE_BIND_POINT_GRAPHICS,
                                   info.pipeline);
     vkCmdBindDescriptorSets(info.cmd, VK_PIPELINE_BIND_POINT_GRAPHICS, info.pipeline_layout,
-            0, NUM_DESCRIPTOR_SETS, info.desc_set.data(), 0, NULL);
+            0, descriptor_set_count, descriptor_sets, 0, NULL);
 
     const VkDeviceSize offsets[1] = {0};
     vkCmdBindVertexBuffers(info.cmd, 0, 1, &info.vertex_buffer.buf, offsets);
@@ -200,18 +312,25 @@ int main(int argc, char **argv)
     assert(res == VK_SUCCESS);
 
     wait_seconds(1);
-    /* VULKAN_KEY_END */
 
     vkDestroySemaphore(info.device, presentCompleteSemaphore);
     destroy_pipeline(info);
     destroy_pipeline_cache(info);
     destroy_texture(info);
-    destroy_descriptor_pool(info);
+
+    // instead of destroy_descriptor_pool(info);
+    vkDestroyDescriptorPool(info.device, descriptor_pool[0]);
+
     destroy_vertex_buffer(info);
     destroy_framebuffers(info);
     destroy_shaders(info);
     destroy_renderpass(info);
-    destroy_descriptor_and_pipeline_layouts(info);
+
+    //instead of destroy_descriptor_and_pipeline_layouts(info);
+    for(int i = 0; i < descriptor_set_count; i++)
+        vkDestroyDescriptorSetLayout(info.device, descriptor_layouts[i]);
+    vkDestroyPipelineLayout(info.device, info.pipeline_layout);
+
     destroy_uniform_buffer(info);
     destroy_depth_buffer(info);
     destroy_swap_chain(info);
