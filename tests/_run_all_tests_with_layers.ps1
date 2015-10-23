392474ceea80a881b2af263c4de5b3bc3f549edf
Tony Barbour <tony@LunarG.com>
2015-10-23 10:54:21 -0600

    tests: Add new layers to _with_layers tests

--- tests/_run_all_tests_with_layers.ps1
+++ tests/_run_all_tests_with_layers.ps1
@@ -11,8 +11,8 @@ echo $dPath
 
 Set-Item -path env:Path -value ($env:Path + ";..\loader\$dPath")
 Set-Item -path env:Path -value ($env:Path + ";gtest-1.7.0\$dPath")
-$env:VK_INSTANCE_LAYERS = "Threading;MemTracker;ObjectTracker;DrawState;ParamChecker;ShaderChecker"
-$env:VK_DEVICE_LAYERS = "Threading;MemTracker;ObjectTracker;DrawState;ParamChecker;ShaderChecker"
+$env:VK_INSTANCE_LAYERS = "Threading;MemTracker;ObjectTracker;DrawState;ParamChecker;ShaderChecker;Swapchain;DeviceLimits;Image"
+$env:VK_DEVICE_LAYERS = "Threading;MemTracker;ObjectTracker;DrawState;ParamChecker;ShaderChecker;Swapchain;DeviceLimits;Image"
 $env:VK_LAYER_PATH = "..\layers\$dPath"
 
 Copy-Item ..\..\tests\vk_layer_settings.txt .
