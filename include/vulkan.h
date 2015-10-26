ddee79bfbcf098446ade4204a24365dc13cb3c13
Chia-I Wu <olv@lunarg.com>
2015-10-26 16:48:28 +0800

    v190: bump API minor version

--- include/vulkan.h
+++ include/vulkan.h
@@ -41,7 +41,7 @@ extern "C" {
     ((major << 22) | (minor << 12) | patch)
 
 // Vulkan API version supported by this file
-#define VK_API_VERSION VK_MAKE_VERSION(0, 189, 0)
+#define VK_API_VERSION VK_MAKE_VERSION(0, 190, 0)
 
 
 #define VK_NULL_HANDLE 0
