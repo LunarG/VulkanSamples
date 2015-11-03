81f95c5245986a6cd91d0606353a1980e3fd54b4
Tony Barbour <tony@LunarG.com>
2015-11-03 16:08:08 -0700

    bug 14543: rename VkTex* enums to VkTexture* similarly for VK_TEX_* to VK_TEXTURE_*

--- src/vk0.9-inittexture.cpp
+++ src/vk0.9-inittexture.cpp
@@ -247,12 +247,12 @@ int main(int argc, char **argv)
 
     VkSamplerCreateInfo samplerCreateInfo = {};
     samplerCreateInfo.sType = VK_STRUCTURE_TYPE_SAMPLER_CREATE_INFO;
-    samplerCreateInfo.magFilter = VK_TEX_FILTER_NEAREST;
-    samplerCreateInfo.minFilter = VK_TEX_FILTER_NEAREST;
-    samplerCreateInfo.mipMode = VK_TEX_MIPMAP_MODE_BASE;
-    samplerCreateInfo.addressModeU = VK_TEX_ADDRESS_MODE_CLAMP;
-    samplerCreateInfo.addressModeV = VK_TEX_ADDRESS_MODE_CLAMP;
-    samplerCreateInfo.addressModeW = VK_TEX_ADDRESS_MODE_CLAMP;
+    samplerCreateInfo.magFilter = VK_FILTER_NEAREST;
+    samplerCreateInfo.minFilter = VK_FILTER_NEAREST;
+    samplerCreateInfo.mipMode = VK_SAMPLER_MIPMAP_MODE_BASE;
+    samplerCreateInfo.addressModeU = VK_SAMPLER_ADDRESS_MODE_CLAMP_TO_EDGE;
+    samplerCreateInfo.addressModeV = VK_SAMPLER_ADDRESS_MODE_CLAMP_TO_EDGE;
+    samplerCreateInfo.addressModeW = VK_SAMPLER_ADDRESS_MODE_CLAMP_TO_EDGE;
     samplerCreateInfo.mipLodBias = 0.0;
     samplerCreateInfo.maxAnisotropy = 0;
     samplerCreateInfo.compareEnable = VK_FALSE;
