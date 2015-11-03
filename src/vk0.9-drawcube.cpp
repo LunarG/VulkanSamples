1f4a366e663facb8816fa46259d89d16e61c0522
Tony Barbour <tony@LunarG.com>
2015-11-03 16:10:52 -0700

    bug 14918 part1: consistency/cleanup ideas

--- src/vk0.9-drawcube.cpp
+++ src/vk0.9-drawcube.cpp
@@ -192,7 +192,7 @@ int main(int argc, char **argv)
     prePresentBarrier.image = info.buffers[info.current_buffer].image;
     VkImageMemoryBarrier *pmemory_barrier = &prePresentBarrier;
     vkCmdPipelineBarrier(info.cmd, VK_PIPELINE_STAGE_ALL_GPU_COMMANDS, VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT,
-                         VK_FALSE, 1, (const void * const*)&pmemory_barrier);
+                         0, 1, (const void * const*)&pmemory_barrier);
 
     res = vkEndCommandBuffer(info.cmd);
     const VkCmdBuffer cmd_bufs[] = { info.cmd };
