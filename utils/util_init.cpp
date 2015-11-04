f8f61f2dec2c4f4514ff7aa7566570d8243eefbd
baldurk <baldurk@baldurk.org>
2015-11-04 20:55:17 +0100

    Render cube at a more interesting angle

--- utils/util_init.cpp
+++ utils/util_init.cpp
@@ -969,7 +969,7 @@ void init_uniform_buffer(struct sample_info &info)
     VkResult U_ASSERT_ONLY res;
     info.Projection = glm::perspective(glm::radians(45.0f), 1.0f, 0.1f, 100.0f);
     info.View       = glm::lookAt(
-                          glm::vec3(0,3,10), // Camera is at (0,3,10), in World Space
+                          glm::vec3(5,3,10), // Camera is at (5,3,10), in World Space
                           glm::vec3(0,0,0), // and looks at the origin
                           glm::vec3(0,-1,0)  // Head is up (set to 0,-1,0 to look upside-down)
                           );
