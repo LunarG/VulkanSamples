af9b63890c80ea4d952ea7ead5c21a5ca4702e66
baldurk <baldurk@baldurk.org>
2015-11-05 14:07:14 +0100

    Handle multiple textures being around

--- src/vk0.10-drawtexturedcube.cpp
+++ src/vk0.10-drawtexturedcube.cpp
@@ -234,7 +234,7 @@ int main(int argc, char **argv)
     vkDestroySemaphore(info.device, presentCompleteSemaphore, NULL);
     destroy_pipeline(info);
     destroy_pipeline_cache(info);
-    destroy_texture(info);
+    destroy_textures(info);
     destroy_descriptor_pool(info);
     destroy_vertex_buffer(info);
     destroy_framebuffers(info);
