f0d7e40ec82620bf99d412cf346b75baea981aea
Tony Barbour <tony@LunarG.com>
2015-11-17 09:15:42 -0700

    Change copyright from LunarG to Valve

--- src/vk0.10-dbgcreatemsgcallback.cpp
+++ src/vk0.10-dbgcreatemsgcallback.cpp
@@ -1,7 +1,7 @@
 /*
  * Vulkan Samples Kit
  *
- * Copyright (C) 2015 LunarG, Inc.
+ * Copyright (C) 2015 Valve Corporation
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
