d04e8df847f27b0635829b3ceeeca13b8446d071
Tony Barbour <tony@LunarG.com>
2015-11-17 10:02:56 -0700

    tests: Add copyright to layer validation tests

--- tests/layer_validation_tests.cpp
+++ tests/layer_validation_tests.cpp
@@ -1,3 +1,33 @@
+//
+//  Copyright (C) 2015 Valve Corporation
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a
+//  copy of this software and associated documentation files (the "Software"),
+//  to deal in the Software without restriction, including without limitation
+//  the rights to use, copy, modify, merge, publish, distribute, sublicense,
+//  and/or sell copies of the Software, and to permit persons to whom the
+//  Software is furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included
+//  in all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+//  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+//  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+//  DEALINGS IN THE SOFTWARE.
+//
+// Author: Chia-I Wu <olvaffe@gmail.com>
+// Author: Chris Forbes <chrisf@ijw.co.nz>
+// Author: Courtney Goeltzenleuchter <courtney@LunarG.com>
+// Author: Mark Lobodzinski <mark@lunarg.com>
+// Author: Mike Stroyan <mike@LunarG.com>
+// Author: Tobin Ehlis <tobin@lunarg.com>
+// Author: Tony Barbour <tony@LunarG.com>
+
+
 #include <vulkan/vulkan.h>
 #include "vulkan/vk_debug_report_lunarg.h"
 #include "test_common.h"
