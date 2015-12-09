59171933cab04dd0b7d18663cb7dc0b47ba8d8be
Tony Barbour <tony@LunarG.com>
2015-12-08 17:26:09 -0700

    Fix check for texelbuffer support

--- src/vk0.10-texelbuffer.cpp
+++ src/vk0.10-texelbuffer.cpp
@@ -84,8 +84,8 @@ int main(int argc, char **argv)
     init_enumerate_device(info);
     init_device(info);
 
-    if (info.gpu_props.limits.maxTexelBufferElements > 0) {
-        std::cout << "maxTexelBufferSize too small\n";
+    if (info.gpu_props.limits.maxTexelBufferElements < 4) {
+        std::cout << "maxTexelBufferElements too small\n";
         exit(-1);
     }
 
