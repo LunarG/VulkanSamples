c4096ab836d7e170ca980e82b28e09d25bf642a4
Cody Northrop <cnorthrop@google.com>
2015-12-11 10:19:41 -0700

    utils: Add get_milliseconds for timing code

--- utils/util.hpp
+++ utils/util.hpp
@@ -252,3 +252,6 @@ bool GLSLtoSPV(const VkShaderStageFlagBits shader_type, const char *pshader, std
 void init_glslang();
 void finalize_glslang();
 void wait_seconds(int seconds);
+
+typedef unsigned long long timestamp_t;
+timestamp_t get_milliseconds();
