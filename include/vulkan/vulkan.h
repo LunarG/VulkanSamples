68dee712cd6dd4798a1b663b9eb1c4422638dc9c
Jon Ashburn <jon@lunarg.com>
2015-12-30 14:09:31 -0700

    bump header version to 213.1
    This includes WSI changes that are post vers 213 so that is why use 213.1

--- include/vulkan/vulkan.h
+++ include/vulkan/vulkan.h
@@ -41,7 +41,7 @@ extern "C" {
     ((major << 22) | (minor << 12) | patch)
 
 // Vulkan API version supported by this file
-#define VK_API_VERSION VK_MAKE_VERSION(0, 212, 1)
+#define VK_API_VERSION VK_MAKE_VERSION(0, 213, 1)
 
 
 #define VK_NULL_HANDLE 0
