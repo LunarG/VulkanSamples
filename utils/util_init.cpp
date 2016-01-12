f6e99978c45722efd72d459b84d3e5b57360276f
Tony Barbour <tony@LunarG.com>
2016-01-12 14:28:45 -0700

    Fix memory tracker validation error
    Specify LOAD_OP_DONT_CARE for swapchain image in begin renderpass

--- utils/util_init.cpp
+++ utils/util_init.cpp
@@ -1137,7 +1137,7 @@ void init_renderpass(struct sample_info &info, bool include_depth, bool clear)
     VkAttachmentDescription attachments[2];
     attachments[0].format = info.format;
     attachments[0].samples = NUM_SAMPLES;
-    attachments[0].loadOp = clear?VK_ATTACHMENT_LOAD_OP_CLEAR:VK_ATTACHMENT_LOAD_OP_LOAD;
+    attachments[0].loadOp = clear?VK_ATTACHMENT_LOAD_OP_CLEAR:VK_ATTACHMENT_LOAD_OP_DONT_CARE;
     attachments[0].storeOp = VK_ATTACHMENT_STORE_OP_STORE;
     attachments[0].stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
     attachments[0].stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
@@ -1149,7 +1149,7 @@ void init_renderpass(struct sample_info &info, bool include_depth, bool clear)
     {
         attachments[1].format = info.depth.format;
         attachments[1].samples = NUM_SAMPLES;
-        attachments[1].loadOp = clear?VK_ATTACHMENT_LOAD_OP_CLEAR:VK_ATTACHMENT_LOAD_OP_LOAD;
+        attachments[1].loadOp = clear?VK_ATTACHMENT_LOAD_OP_CLEAR:VK_ATTACHMENT_LOAD_OP_DONT_CARE;
         attachments[1].storeOp = VK_ATTACHMENT_STORE_OP_STORE;
         attachments[1].stencilLoadOp = VK_ATTACHMENT_LOAD_OP_LOAD;
         attachments[1].stencilStoreOp = VK_ATTACHMENT_STORE_OP_STORE;
