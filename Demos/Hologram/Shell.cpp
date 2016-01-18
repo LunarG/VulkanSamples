bdfa3e261a0f644f3f3df792359543b2d78732a0
Chia-I Wu <olv@google.com>
2016-01-18 14:36:24 +0800

    Hologram: set a limit on ticks per add_game_time call
    If on_tick and on_frame combined took too long to run, we would prefer
    catching up slowly than having a big jump.

--- Demos/Hologram/Shell.cpp
+++ Demos/Hologram/Shell.cpp
@@ -394,10 +394,12 @@ void Shell::resize_swapchain(uint32_t width_hint, uint32_t height_hint)
 
 void Shell::add_game_time(float time)
 {
+    int max_ticks = 3;
+
     if (!settings_.no_tick)
         game_time_ += time;
 
-    while (game_time_ >= game_tick_) {
+    while (game_time_ >= game_tick_ && max_ticks--) {
         game_.on_tick();
         game_time_ -= game_tick_;
     }
