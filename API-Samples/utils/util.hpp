bc493a2b91b343904f9dd3d731af92f4d1274065
Hak Matsuda <hakuro@google.com>
2016-01-21 11:44:56 -0800

    Retrieve the window size via helper function.

--- API-Samples/utils/util.hpp
+++ API-Samples/utils/util.hpp
@@ -251,3 +251,7 @@ timestamp_t get_milliseconds();
 // Main entry point of samples
 int sample_main();
 
+// Helper to retrieve Window size.
+bool get_window_size(int32_t* width, int32_t* height);
+
+
