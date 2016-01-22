c23afb40a14de1ae139b818e282ad2dab4a4d035
Hak Matsuda <hakuro@google.com>
2016-01-22 14:14:02 -0800

    Revert "Replace wait_seconds() to a stub function."
    This reverts commit 04a979a27aee9082a86d5ac49ec429c7c9997049.

--- API-Samples/utils/util.hpp
+++ API-Samples/utils/util.hpp
@@ -254,8 +254,4 @@ int sample_main();
 // Helper to retrieve Window size.
 bool get_window_size(int32_t* width, int32_t* height);
 
-// Wait a specified time in second.
-bool wait(int32_t timeout);
-
-
 
