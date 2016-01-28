c86ca44f9dd8b1c060f8b0d15272352625f93eec
Chia-I Wu <olv@google.com>
2016-01-29 07:45:15 +0800

    Hologram: move Animation into Simulation

--- Demos/Hologram/Simulation.h
+++ Demos/Hologram/Simulation.h
@@ -6,8 +6,31 @@
 
 #include <glm/glm.hpp>
 
-#include "Animation.h"
 #include "Path.h"
+
+class Animation {
+public:
+    Animation(unsigned rng_seed);
+
+    glm::mat4 transformation(float t);
+
+private:
+    struct Data {
+        glm::vec3 axis;
+        float speed;
+        float scale;
+
+        float time;
+        glm::mat4 matrix;
+    };
+
+    std::mt19937 rng_;
+    std::uniform_real_distribution<float> dir_;
+    std::uniform_real_distribution<float> speed_;
+    std::uniform_real_distribution<float> scale_;
+
+    Data current_;
+};
 
 class Simulation {
 public:
