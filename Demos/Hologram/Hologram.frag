9290bdf2906cb9fa06c8e00477b168e3aa3c1dee
Chia-I Wu <olv@google.com>
2016-02-02 09:18:49 +0800

    Hologram: rework Meshes
    Upload only one pyramid mesh into the mesh buffers.  Specify normals
    instead of colors.  As a result, all pyramids are the same and that will
    be fixed later.

--- Demos/Hologram/Hologram.frag
+++ Demos/Hologram/Hologram.frag
@@ -2,10 +2,11 @@
 
 precision highp float;
 
-in vec4 color;
+in vec3 color;
+
 layout(location = 0) out vec4 fragcolor;
 
 void main()
 {
-	fragcolor = color;
+	fragcolor = vec4(color, 0.5);
 }
--- Demos/Hologram/Meshes.cpp
+++ Demos/Hologram/Meshes.cpp
@@ -22,125 +22,74 @@
 
 #include <cassert>
 #include <cstring>
-#include <random>
+#include <array>
 
 #include "Helpers.h"
 #include "Meshes.h"
 
-class MeshGenerator {
+namespace {
+
+class Mesh {
 public:
-    MeshGenerator(unsigned int rng_seed, int count) :
-        count_(count), current_(-1), rng_(rng_seed), color_(0.0f, 1.0f)
+    struct Position {
+        float x;
+        float y;
+        float z;
+    };
+
+    struct Normal {
+        float x;
+        float y;
+        float z;
+    };
+
+    struct Face {
+        int v0;
+        int v1;
+        int v2;
+    };
+
+    static uint32_t vertex_stride()
     {
+        // Position + Normal
+        const int comp_count = 6;
+
+        return sizeof(float) * comp_count;
     }
 
-    void head()
-    {
-        current_ = -1;
-    }
-
-    bool next()
-    {
-        if (current_ >= count_ - 1)
-            return false;
-
-        current_++;
-
-        return true;
-    }
-
-    uint32_t vertex_count()
-    {
-        return 5;
-    }
-
-    uint32_t vertex_size()
-    {
-        // vec3 + vec4
-        return sizeof(float) * 7;
-    }
-
-    VkDeviceSize write_vertices(void *data, VkDeviceSize size)
-    {
-        float vb_data[5][7] = {
-            //        pos                      color
-            {  0.0f,  0.0f,  1.0f,  1.0f,  1.0f,  1.0f, 0.5f },
-            { -1.0f, -1.0f, -1.0f,  1.0f,  0.0f,  0.0f, 0.5f },
-            {  1.0f, -1.0f, -1.0f,  0.0f,  1.0f,  0.0f, 0.5f },
-            {  1.0f,  1.0f, -1.0f,  0.0f,  0.0f,  1.0f, 0.5f },
-            { -1.0f,  1.0f, -1.0f,  1.0f,  1.0f,  0.0f, 0.5f },
-        };
-
-        for (int i = 0; i < 5; i++) {
-            vb_data[i][3] = color_(rng_);
-            vb_data[i][4] = color_(rng_);
-            vb_data[i][5] = color_(rng_);
-            vb_data[i][6] = color_(rng_);
-        }
-
-        assert(sizeof(vb_data) <= size);
-        std::memcpy(data, vb_data, sizeof(vb_data));
-        return sizeof(vb_data);
-    }
-
-    uint32_t index_count()
-    {
-        // six triangles
-        return 3 * 6;
-    }
-
-    VkDeviceSize index_size()
-    {
-        return sizeof(uint32_t);
-    }
-
-    VkIndexType index_type()
-    {
-        return VK_INDEX_TYPE_UINT32;
-    }
-
-    VkDeviceSize write_indices(void *data, VkDeviceSize size)
-    {
-        static const uint32_t ib_data[18] = {
-            0, 1, 2,
-            0, 2, 3,
-            0, 3, 4,
-            0, 4, 1,
-            1, 4, 3,
-            1, 3, 2,
-        };
-
-        assert(sizeof(ib_data) <= size);
-        std::memcpy(data, ib_data, sizeof(ib_data));
-        return sizeof(ib_data);
-    }
-
-    std::vector<VkVertexInputBindingDescription> vertex_input_bindings()
+    static VkVertexInputBindingDescription vertex_input_binding()
     {
         VkVertexInputBindingDescription vi_binding = {};
         vi_binding.binding = 0;
-        vi_binding.stride = vertex_size();
+        vi_binding.stride = vertex_stride();
         vi_binding.inputRate = VK_VERTEX_INPUT_RATE_VERTEX;
 
-        return std::vector<VkVertexInputBindingDescription>(1, vi_binding);
+        return vi_binding;
     }
 
-    std::vector<VkVertexInputAttributeDescription> vertex_input_attributes()
+    static std::vector<VkVertexInputAttributeDescription> vertex_input_attributes()
     {
-        VkVertexInputAttributeDescription vi_attrs[2] = {};
+        std::vector<VkVertexInputAttributeDescription> vi_attrs(2);
+        // Position
         vi_attrs[0].location = 0;
         vi_attrs[0].binding = 0;
         vi_attrs[0].format = VK_FORMAT_R32G32B32_SFLOAT;
         vi_attrs[0].offset = 0;
+        // Normal
         vi_attrs[1].location = 1;
         vi_attrs[1].binding = 0;
-        vi_attrs[1].format = VK_FORMAT_R32G32B32A32_SFLOAT;
+        vi_attrs[1].format = VK_FORMAT_R32G32B32_SFLOAT;
         vi_attrs[1].offset = sizeof(float) * 3;
 
-        return std::vector<VkVertexInputAttributeDescription>(vi_attrs, vi_attrs + 2);
+        return vi_attrs;
     }
 
-    VkPipelineInputAssemblyStateCreateInfo input_assembly_state()
+    static VkIndexType index_type()
+    {
+        return VK_INDEX_TYPE_UINT32;
+    }
+
+    static VkPipelineInputAssemblyStateCreateInfo input_assembly_state()
     {
         VkPipelineInputAssemblyStateCreateInfo ia_info = {};
         ia_info.sType = VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO;
@@ -149,18 +98,158 @@ public:
         return ia_info;
     }
 
-private:
-    int count_;
-    int current_;
+    void build(const std::vector<std::array<float, 6>> &vertices, const std::vector<std::array<int, 3>> &faces)
+    {
+        positions_.reserve(vertices.size());
+        normals_.reserve(vertices.size());
+        for (const auto &v : vertices) {
+            positions_.emplace_back(Position{ v[0], v[1], v[2] });
+            normals_.emplace_back(Normal{ v[3], v[4], v[5] });
+        }
 
-    std::mt19937 rng_;
-    std::uniform_real_distribution<float> color_;
+        faces_.reserve(faces.size());
+        for (const auto &f : faces)
+            faces_.emplace_back(Face{ f[0], f[1], f[2] });
+    }
+
+    uint32_t vertex_count() const
+    {
+        return positions_.size();
+    }
+
+    VkDeviceSize vertex_buffer_size() const
+    {
+        return vertex_stride() * vertex_count();
+    }
+
+    void vertex_buffer_write(void *data) const
+    {
+        float *dst = reinterpret_cast<float *>(data);
+        for (size_t i = 0; i < positions_.size(); i++) {
+            const Position &pos = positions_[i];
+            const Normal &normal = normals_[i];
+            dst[0] = pos.x;
+            dst[1] = pos.y;
+            dst[2] = pos.z;
+            dst[3] = normal.x;
+            dst[4] = normal.y;
+            dst[5] = normal.z;
+            dst += 6;
+        }
+    }
+
+    uint32_t index_count() const
+    {
+        return faces_.size() * 3;
+    }
+
+    VkDeviceSize index_buffer_size() const
+    {
+        return sizeof(uint32_t) * index_count();
+    }
+
+    void index_buffer_write(void *data) const
+    {
+        uint32_t *dst = reinterpret_cast<uint32_t *>(data);
+        for (const auto &face : faces_) {
+            dst[0] = face.v0;
+            dst[1] = face.v1;
+            dst[2] = face.v2;
+            dst += 3;
+        }
+    }
+
+    std::vector<Position> positions_;
+    std::vector<Normal> normals_;
+    std::vector<Face> faces_;
 };
 
-Meshes::Meshes(unsigned int rng_seed, const std::vector<VkMemoryPropertyFlags> &mem_flags, VkDevice dev, int count)
-    : dev_(dev), vertex_input_state_(), input_assembly_state_()
+class BuildPyramid {
+public:
+    BuildPyramid(Mesh &mesh)
+    {
+        const std::vector<std::array<float, 6>> vertices = {
+            //      position                normal
+            {  0.0f,  0.0f,  1.0f,    0.0f,  0.0f,  1.0f },
+            { -1.0f, -1.0f, -1.0f,   -1.0f, -1.0f, -1.0f },
+            {  1.0f, -1.0f, -1.0f,    1.0f, -1.0f, -1.0f },
+            {  1.0f,  1.0f, -1.0f,    1.0f,  1.0f, -1.0f },
+            { -1.0f,  1.0f, -1.0f,   -1.0f,  1.0f, -1.0f },
+        };
+
+        const std::vector<std::array<int, 3>> faces = {
+            { 0, 1, 2 },
+            { 0, 2, 3 },
+            { 0, 3, 4 },
+            { 0, 4, 1 },
+            { 1, 4, 3 },
+            { 1, 3, 2 },
+        };
+
+        mesh.build(vertices, faces);
+    }
+};
+
+void build_meshes(std::array<Mesh, Meshes::MESH_COUNT> &meshes)
 {
-    generate(rng_seed, mem_flags, count);
+    BuildPyramid build_pyramid(meshes[Meshes::MESH_PYRAMID]);
+}
+
+} // namespace
+
+Meshes::Meshes(VkDevice dev, const std::vector<VkMemoryPropertyFlags> &mem_flags)
+    : dev_(dev),
+      vertex_input_binding_(Mesh::vertex_input_binding()),
+      vertex_input_attrs_(Mesh::vertex_input_attributes()),
+      vertex_input_state_(),
+      input_assembly_state_(Mesh::input_assembly_state()),
+      index_type_(Mesh::index_type())
+{
+    vertex_input_state_.sType = VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO;
+    vertex_input_state_.vertexBindingDescriptionCount = 1;
+    vertex_input_state_.pVertexBindingDescriptions = &vertex_input_binding_;
+    vertex_input_state_.vertexAttributeDescriptionCount = static_cast<uint32_t>(vertex_input_attrs_.size());
+    vertex_input_state_.pVertexAttributeDescriptions = vertex_input_attrs_.data();
+
+    std::array<Mesh, MESH_COUNT> meshes;
+    build_meshes(meshes);
+
+    draw_commands_.reserve(meshes.size());
+    uint32_t first_index = 0;
+    int32_t vertex_offset = 0;
+    VkDeviceSize vb_size = 0;
+    VkDeviceSize ib_size = 0;
+    for (const auto &mesh : meshes) {
+        VkDrawIndexedIndirectCommand draw = {};
+        draw.indexCount = mesh.index_count();
+        draw.instanceCount = 1;
+        draw.firstIndex = first_index;
+        draw.vertexOffset = vertex_offset;
+        draw.firstInstance = 0;
+
+        draw_commands_.push_back(draw);
+
+        first_index += mesh.index_count();
+        vertex_offset += mesh.vertex_count();
+        vb_size += mesh.vertex_buffer_size();
+        ib_size += mesh.index_buffer_size();
+    }
+
+    allocate_resources(vb_size, ib_size, mem_flags);
+
+    uint8_t *vb_data, *ib_data;
+    vk::assert_success(vk::MapMemory(dev_, mem_, 0, VK_WHOLE_SIZE,
+                0, reinterpret_cast<void **>(&vb_data)));
+    ib_data = vb_data + ib_mem_offset_;
+
+    for (const auto &mesh : meshes) {
+        mesh.vertex_buffer_write(vb_data);
+        mesh.index_buffer_write(ib_data);
+        vb_data += mesh.vertex_buffer_size();
+        ib_data += mesh.index_buffer_size();
+    }
+
+    vk::UnmapMemory(dev_, mem_);
 }
 
 Meshes::~Meshes()
@@ -178,60 +267,23 @@ void Meshes::cmd_bind_buffers(VkCommandBuffer cmd) const
     vk::CmdBindIndexBuffer(cmd, ib_, 0, index_type_);
 }
 
-void Meshes::cmd_draw(VkCommandBuffer cmd, int mesh) const
+void Meshes::cmd_draw(VkCommandBuffer cmd, Type type) const
 {
-    const Mesh &m = meshes_[mesh];
-    vk::CmdDrawIndexed(cmd, m.index_count, 1, m.first_index, m.vertex_offset, 0);
+    const auto &draw = draw_commands_[type];
+    vk::CmdDrawIndexed(cmd, draw.indexCount, draw.instanceCount,
+            draw.firstIndex, draw.vertexOffset, draw.firstInstance);
 }
 
-void Meshes::generate(unsigned int rng_seed, const std::vector<VkMemoryPropertyFlags> &mem_flags, int count)
-{
-    MeshGenerator gen(rng_seed, count);
-
-    meshes_.reserve(count);
-
-    uint32_t vertex_total = 0;
-    uint32_t index_total = 0;
-    gen.head();
-    while (gen.next()) {
-        Mesh m;
-        m.index_count = gen.index_count();
-        m.first_index = index_total;
-        m.vertex_offset = vertex_total;
-        meshes_.push_back(m);
-
-        vertex_total += gen.vertex_count();
-        index_total += m.index_count;
-    }
-
-    vertex_input_bindings_ = gen.vertex_input_bindings();
-    vertex_input_attrs_ = gen.vertex_input_attributes();
-    vertex_input_state_.sType = VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO;
-    vertex_input_state_.vertexBindingDescriptionCount = (uint32_t)vertex_input_bindings_.size();
-    vertex_input_state_.pVertexBindingDescriptions = vertex_input_bindings_.data();
-    vertex_input_state_.vertexAttributeDescriptionCount = (uint32_t)vertex_input_attrs_.size();
-    vertex_input_state_.pVertexAttributeDescriptions = vertex_input_attrs_.data();
-
-    input_assembly_state_ = gen.input_assembly_state();
-
-    index_type_ = gen.index_type();
-    vb_size_ = gen.vertex_size() * vertex_total;
-    ib_size_ = gen.index_size() * index_total;
-
-    allocate_resources(mem_flags);
-    upload_meshes(gen);
-}
-
-void Meshes::allocate_resources(const std::vector<VkMemoryPropertyFlags> &mem_flags)
+void Meshes::allocate_resources(VkDeviceSize vb_size, VkDeviceSize ib_size, const std::vector<VkMemoryPropertyFlags> &mem_flags)
 {
     VkBufferCreateInfo buf_info = {};
     buf_info.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
-    buf_info.size = vb_size_;
+    buf_info.size = vb_size;
     buf_info.usage = VK_BUFFER_USAGE_VERTEX_BUFFER_BIT;
     buf_info.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
     vk::CreateBuffer(dev_, &buf_info, nullptr, &vb_);
 
-    buf_info.size = ib_size_;
+    buf_info.size = ib_size;
     buf_info.usage = VK_BUFFER_USAGE_INDEX_BUFFER_BIT;
     vk::CreateBuffer(dev_, &buf_info, nullptr, &ib_);
 
@@ -263,26 +315,4 @@ void Meshes::allocate_resources(const std::vector<VkMemoryPropertyFlags> &mem_fl
 
     vk::BindBufferMemory(dev_, vb_, mem_, 0);
     vk::BindBufferMemory(dev_, ib_, mem_, ib_mem_offset_);
-}
-
-void Meshes::upload_meshes(MeshGenerator &gen)
-{
-    uint8_t *vb_data, *ib_data;
-    vk::MapMemory(dev_, mem_, 0, VK_WHOLE_SIZE, 0, reinterpret_cast<void **>(&vb_data));
-    ib_data = vb_data + ib_mem_offset_;
-
-    VkDeviceSize vb_avail = vb_size_;
-    VkDeviceSize ib_avail = ib_size_;
-    gen.head();
-    while (gen.next()) {
-        VkDeviceSize written = gen.write_vertices(vb_data, vb_avail);
-        vb_data += written;
-        vb_avail -= written;
-
-        written = gen.write_indices(ib_data, ib_avail);
-        ib_data += written;
-        ib_avail -= written;
-    }
-
-    vk::UnmapMemory(dev_, mem_);
 }
