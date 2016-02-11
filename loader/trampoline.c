6a6048fddfa026f0afce4744ef4414395229b78b
Jon Ashburn <jon@lunarg.com>
2016-02-10 20:50:19 -0700

    loader: rerun clang-format on some files

--- loader/trampoline.c
+++ loader/trampoline.c
@@ -106,10 +106,10 @@ vkCreateInstance(const VkInstanceCreateInfo *pCreateInfo,
 
     /* validate the app requested layers to be enabled */
     if (pCreateInfo->enabledLayerCount > 0) {
-        res = loader_validate_layers(ptr_instance,
-                                     pCreateInfo->enabledLayerCount,
-                                     pCreateInfo->ppEnabledLayerNames,
-                                     &ptr_instance->instance_layer_list);
+        res =
+            loader_validate_layers(ptr_instance, pCreateInfo->enabledLayerCount,
+                                   pCreateInfo->ppEnabledLayerNames,
+                                   &ptr_instance->instance_layer_list);
         if (res != VK_SUCCESS) {
             util_DestroyDebugReportCallback(ptr_instance, instance_callback,
                                             NULL);
@@ -127,8 +127,8 @@ vkCreateInstance(const VkInstanceCreateInfo *pCreateInfo,
     loader_get_icd_loader_instance_extensions(
         ptr_instance, &ptr_instance->icd_libs, &ptr_instance->ext_list);
     res = loader_validate_instance_extensions(
-        ptr_instance, &ptr_instance->ext_list, &ptr_instance->instance_layer_list,
-        pCreateInfo);
+        ptr_instance, &ptr_instance->ext_list,
+        &ptr_instance->instance_layer_list, pCreateInfo);
     if (res != VK_SUCCESS) {
         loader_delete_layer_properties(ptr_instance,
                                        &ptr_instance->device_layer_list);
