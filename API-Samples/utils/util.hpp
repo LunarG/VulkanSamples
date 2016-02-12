d714474be0edcef08faa04d886659383f18d7c46
Tony Barbour <tony@LunarG.com>
2016-02-12 13:20:54 -0700

    Ensure texture image is in LAYOUT_GENERAL before mapping

--- API-Samples/utils/util.hpp
+++ API-Samples/utils/util.hpp
@@ -200,6 +200,8 @@ struct sample_info {
     struct {
         VkDescriptorImageInfo image_info;
     } texture_data;
+    VkDeviceMemory stagingMemory;
+    VkImage stagingImage;
 
     struct {
         VkBuffer buf;
