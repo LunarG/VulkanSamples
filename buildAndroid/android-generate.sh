87c2b9d44bd93e87324d810c98569f7030005110
Mun, Gwan-gyeong <kk.moon@samsung.com>
2016-02-22 20:29:23 +0900

    Build: GH PR17, Add Android WSI info to android codegen script
    Conflicts:
    	buildAndroid/android-generate.sh

--- buildAndroid/android-generate.sh
+++ buildAndroid/android-generate.sh
@@ -17,13 +17,13 @@
 
 rm -rf generated
 mkdir -p generated
-python ../vk-generate.py dispatch-table-ops layer > generated/vk_dispatch_table_helper.h
+python ../vk-generate.py Android dispatch-table-ops layer > generated/vk_dispatch_table_helper.h
 
 python ../vk_helper.py --gen_enum_string_helper ../include/vulkan/vulkan.h --abs_out_dir generated
 python ../vk_helper.py --gen_struct_wrappers ../include/vulkan/vulkan.h --abs_out_dir generated
 
-python ../vk-layer-generate.py object_tracker ../include/vulkan/vulkan.h > generated/object_tracker.cpp
-python ../vk-layer-generate.py unique_objects ../include/vulkan/vulkan.h > generated/unique_objects.cpp
+python ../vk-layer-generate.py Android object_tracker ../include/vulkan/vulkan.h > generated/object_tracker.cpp
+python ../vk-layer-generate.py Android unique_objects ../include/vulkan/vulkan.h > generated/unique_objects.cpp
 ( cd generated; python ../../genvk.py threading -registry ../../vk.xml thread_check.h )
 ( cd generated; python ../../genvk.py paramchecker -registry ../../vk.xml param_check.h )
 
