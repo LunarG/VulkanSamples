09bc70cd5eb6e4968823c6c3607978c73d809a84
Michael Lentine <mlentine@google.com>
2016-02-29 14:22:59 -0600

    layers: Format updates for attachments overlapping memory code in draw_state

--- layers/draw_state.h
+++ layers/draw_state.h
@@ -676,7 +676,8 @@ typedef struct _GLOBAL_CB_NODE {
     unordered_map<QueryObject, bool> queryToStateMap; // 0 is unavailable, 1 is available
     unordered_set<QueryObject> activeQueries;
     unordered_set<QueryObject> startedQueries;
-    unordered_map<ImageSubresourcePair, IMAGE_CMD_BUF_LAYOUT_NODE> imageLayoutMap;
+    unordered_map<ImageSubresourcePair, IMAGE_CMD_BUF_LAYOUT_NODE>
+        imageLayoutMap;
     unordered_map<VkImage, vector<ImageSubresourcePair>> imageSubresourceMap;
     unordered_map<VkEvent, VkPipelineStageFlags> eventToStageMap;
     vector<DRAW_DATA> drawData;
