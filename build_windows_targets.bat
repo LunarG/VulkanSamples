1fb5bec02707ac746fe788ecb634c940681d2bfd
Tony Barbour <tony@LunarG.com>
2016-03-08 14:13:40 -0700

    Fix windows build script to accomodate Overlay layer dependency
    Change-Id: I6456b28f448f029c9afeca52f03b8c32307f1eaa

--- build_windows_targets.bat
+++ build_windows_targets.bat
@@ -18,7 +18,7 @@ rmdir /Q /S build
 rmdir /Q /S build32
 
 REM *******************************************
-REM 64-bit LoaderAndTools build
+REM 64-bit Samples build
 REM *******************************************
 mkdir build
 pushd build
@@ -26,54 +26,74 @@ pushd build
 echo Generating 64-bit spirv-tools CMake files for Visual Studio %VS_VERSION%
 cmake -G "Visual Studio %VS_VERSION% Win64" ..
    
-echo Building 64-bit Debug LoaderAndTools 
+echo Building 64-bit Debug Samples 
+REM msbuild doesn't seem to see the dependency between the Overlay sample and layer_utils
+cd layers
+msbuild generate_vk_layer_helpers.vcxproj /p:Platform=x64 /p:Configuration=Debug /verbosity:quiet
+msbuild layer_utils_static.vcxproj /p:Platform=x64 /p:Configuration=Debug /verbosity:quiet
+cd ..
 msbuild ALL_BUILD.vcxproj /p:Platform=x64 /p:Configuration=Debug /verbosity:quiet
    
 REM Check for existence of one DLL, even though we should check for all results
 if not exist .\loader\Debug\vulkan-1.dll (
    echo.
-   echo LoaderAndTools 64-bit Debug build failed!
+   echo Samples 64-bit Debug build failed!
    set errorCode=1
 )
    
-echo Building 64-bit Release LoaderAndTools 
+echo Building 64-bit Release Samples
+REM msbuild doesn't seem to see the dependency between the Overlay sample and layer_utils
+cd layers
+msbuild generate_vk_layer_helpers.vcxproj /p:Platform=x64 /p:Configuration=Release /verbosity:quiet
+msbuild layer_utils_static.vcxproj /p:Platform=x64 /p:Configuration=Release /verbosity:quiet
+cd ..
 msbuild ALL_BUILD.vcxproj /p:Platform=x64 /p:Configuration=Release /verbosity:quiet
 
 REM Check for existence of one DLL, even though we should check for all results
 if not exist .\loader\Release\vulkan-1.dll (
    echo.
-   echo LoaderAndTools 64-bit Release build failed!
+   echo Samples 64-bit Release build failed!
    set errorCode=1
 )
 
 popd
  
 REM *******************************************
-REM 32-bit LoaderAndTools build
+REM 32-bit Samples build
 REM *******************************************
 mkdir build32
 pushd build32
   
-echo Generating 32-bit LoaderAndTools CMake files for Visual Studio %VS_VERSION%
+echo Generating 32-bit Samples CMake files for Visual Studio %VS_VERSION%
 cmake -G "Visual Studio %VS_VERSION%" ..
    
-echo Building 32-bit Debug LoaderAndTools 
+echo Building 32-bit Debug Samples
+REM msbuild doesn't seem to see the dependency between the Overlay sample and layer_utils
+cd layers
+msbuild generate_vk_layer_helpers.vcxproj /p:Platform=x86 /p:Configuration=Debug /verbosity:quiet
+msbuild layer_utils_static.vcxproj /p:Platform=x86 /p:Configuration=Debug /verbosity:quiet
+cd ..
 msbuild ALL_BUILD.vcxproj /p:Platform=x86 /p:Configuration=Debug /verbosity:quiet
    
 REM Check for existence of one DLL, even though we should check for all results
 if not exist .\loader\Debug\vulkan-1.dll (
    echo.
-   echo LoaderAndTools 32-bit Debug build failed!
+   echo Samples 32-bit Debug build failed!
    set errorCode=1
 )
    
-echo Building 32-bit Release LoaderAndTools 
+echo Building 32-bit Release Samples
+REM msbuild doesn't seem to see the dependency between the Overlay sample and layer_utils
+cd layers
+msbuild generate_vk_layer_helpers.vcxproj /p:Platform=x86 /p:Configuration=Release /verbosity:quiet
+msbuild layer_utils_static.vcxproj /p:Platform=x86 /p:Configuration=Release /verbosity:quiet
+cd ..
 msbuild ALL_BUILD.vcxproj /p:Platform=x86 /p:Configuration=Release /verbosity:quiet
 
 REM Check for existence of one DLL, even though we should check for all results
 if not exist .\loader\Release\vulkan-1.dll (
    echo.
-   echo LoaderAndTools 32-bit Release build failed!
+   echo Samples 32-bit Release build failed!
    set errorCode=1
 )
 
