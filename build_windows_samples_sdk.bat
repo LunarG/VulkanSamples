615f5b81819a84c03fb3a5f1c1be22941aaed7cf
Tony Barbour <tony@LunarG.com>
2016-03-15 14:13:10 -0600

    Add batch script to build the samples in the Windows SDK
    Change-Id: I3da14e459ac1928b705d1b0be5dfa895b33fc54f

---
+++ build_windows_samples_sdk.bat
@@ -0,0 +1,19 @@
+cd %VULKAN_SDK%
+cd glslang
+md build
+cd build
+cmake -G "Visual Studio 12 Win64" ..
+msbuild ALL_BUILD.vcxproj /p:Platform=x64 /p:Configuration=Debug /verbosity:quiet
+cd ..\..
+cd spirv-tools
+md build
+cd build
+cmake -G "Visual Studio 12 Win64" ..
+msbuild ALL_BUILD.vcxproj /p:Platform=x64 /p:Configuration=Debug /verbosity:quiet
+cd ..\..
+cd samples
+md build
+cd build
+cmake -G "Visual Studio 12 Win64" ..
+msbuild ALL_BUILD.vcxproj /p:Platform=x64 /p:Configuration=Debug /verbosity:quiet
+
