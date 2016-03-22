4e01ddc148385302a57077a3346df83409722fc5
Karl Schultz <karl@lunarg.com>
2016-03-22 12:05:31 -0600

    build: Cleanup build_windows_targets.bat
    - This batch file can be run in any of the three repos,
      Vulkan-LoaderAndValidationLayers, VulkanTools, and VulkanSamples,
      so remove comments suggesting otherwise.
    - Replace spot-check for an artifact with checking errorlevel
      from msbuild to discover build failures.

--- build_windows_targets.bat
+++ build_windows_targets.bat
@@ -1,8 +1,14 @@
 echo off
 REM
-REM This batch file builds both 32-bit and 64-bit versions of the loader.
-REM It is assumed that the developer has run the update_external_sources.bat
-REM file prior to running this.
+REM This Windows batch file builds this repository for the following targets:
+REM 64-bit Debug
+REM 64-bit Release
+REM 32-bit Debug
+REM 32-bit Release
+REM It uses CMake to genererate the project files and then invokes msbuild
+REM to build them.
+REM The update_external_sources.bat batch file must be executed before running
+REM this batch file
 REM
 
 REM Determine the appropriate CMake strings for the current version of Visual Studio
@@ -10,72 +16,66 @@ echo Determining VS version
 python .\determine_vs_version.py > vsversion.tmp
 set /p VS_VERSION=< vsversion.tmp
 echo Detected Visual Studio Version as %VS_VERSION%
-
-REM Cleanup the file we used to collect the VS version output since it's no longer needed.
 del /Q /F vsversion.tmp
 
 rmdir /Q /S build
 rmdir /Q /S build32
 
 REM *******************************************
-REM 64-bit LoaderAndTools build
+REM 64-bit build
 REM *******************************************
 mkdir build
 pushd build
 
-echo Generating 64-bit spirv-tools CMake files for Visual Studio %VS_VERSION%
+echo Generating 64-bit CMake files for Visual Studio %VS_VERSION%
 cmake -G "Visual Studio %VS_VERSION% Win64" ..
    
-echo Building 64-bit Debug LoaderAndTools 
+echo Building 64-bit Debug 
 msbuild ALL_BUILD.vcxproj /p:Platform=x64 /p:Configuration=Debug /verbosity:quiet
-   
-REM Check for existence of one DLL, even though we should check for all results
-if not exist .\loader\Debug\vulkan-1.dll (
+if errorlevel 1 (
    echo.
-   echo LoaderAndTools 64-bit Debug build failed!
-   set errorCode=1
-)
+   echo 64-bit Debug build failed!
+   popd
+   exit /B 1
+)   
    
-echo Building 64-bit Release LoaderAndTools 
+echo Building 64-bit Release 
 msbuild ALL_BUILD.vcxproj /p:Platform=x64 /p:Configuration=Release /verbosity:quiet
-
-REM Check for existence of one DLL, even though we should check for all results
-if not exist .\loader\Release\vulkan-1.dll (
+if errorlevel 1 (
    echo.
-   echo LoaderAndTools 64-bit Release build failed!
-   set errorCode=1
-)
+   echo 64-bit Release build failed!
+   popd
+   exit /B 1
+)   
 
 popd
  
 REM *******************************************
-REM 32-bit LoaderAndTools build
+REM 32-bit build
 REM *******************************************
 mkdir build32
 pushd build32
   
-echo Generating 32-bit LoaderAndTools CMake files for Visual Studio %VS_VERSION%
+echo Generating 32-bit CMake files for Visual Studio %VS_VERSION%
 cmake -G "Visual Studio %VS_VERSION%" ..
    
-echo Building 32-bit Debug LoaderAndTools 
+echo Building 32-bit Debug 
 msbuild ALL_BUILD.vcxproj /p:Platform=x86 /p:Configuration=Debug /verbosity:quiet
-   
-REM Check for existence of one DLL, even though we should check for all results
-if not exist .\loader\Debug\vulkan-1.dll (
+if errorlevel 1 (
    echo.
-   echo LoaderAndTools 32-bit Debug build failed!
-   set errorCode=1
-)
+   echo 32-bit Debug build failed!
+   popd
+   exit /B 1
+)   
    
-echo Building 32-bit Release LoaderAndTools 
+echo Building 32-bit Release 
 msbuild ALL_BUILD.vcxproj /p:Platform=x86 /p:Configuration=Release /verbosity:quiet
-
-REM Check for existence of one DLL, even though we should check for all results
-if not exist .\loader\Release\vulkan-1.dll (
+if errorlevel 1 (
    echo.
-   echo LoaderAndTools 32-bit Release build failed!
-   set errorCode=1
-)
+   echo 32-bit Release build failed!
+   popd
+   exit /B 1
+)   
 
 popd
-
+exit /b 0
