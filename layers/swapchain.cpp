e4097b5250c36e70f14f7faa7a7c27b8dbd18c65
Ian Elliott <ianelliott@google.com>
2016-03-23 11:13:09 -0600

    layers: GH117: Change warning to an error.

--- layers/swapchain.cpp
+++ layers/swapchain.cpp
@@ -1744,9 +1744,9 @@ VK_LAYER_EXPORT VKAPI_ATTR VkResult VKAPI_CALL vkAcquireNextImageKHR(VkDevice de
                               VK_KHR_SWAPCHAIN_EXTENSION_NAME);
     }
     if ((semaphore == VK_NULL_HANDLE) && (fence == VK_NULL_HANDLE)) {
-        skipCall |= LOG_WARNING(VK_DEBUG_REPORT_OBJECT_TYPE_DEVICE_EXT, device, "VkDevice", SWAPCHAIN_NO_SYNC_FOR_ACQUIRE,
-                                "%s() called with both the semaphore and fence parameters set to "
-                                "VK_NULL_HANDLE (at least one should be used).", __FUNCTION__);
+        skipCall |= LOG_ERROR(VK_DEBUG_REPORT_OBJECT_TYPE_DEVICE_EXT, device, "VkDevice", SWAPCHAIN_NO_SYNC_FOR_ACQUIRE,
+                              "%s() called with both the semaphore and fence parameters set to "
+                              "VK_NULL_HANDLE (at least one should be used).", __FUNCTION__);
     }
     SwpSwapchain *pSwapchain = &my_data->swapchainMap[swapchain];
     if (pSwapchain) {
