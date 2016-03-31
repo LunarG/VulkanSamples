37419f56a9c618288bf2aa06d62920aade33c37e
guanghuafan <gfan@google.com>
2016-03-31 12:02:44 -0700

    android: fixed issues in windows build
    tested: windows/mac

--- build-android/android-generate.bat
+++ build-android/android-generate.bat
@@ -47,24 +47,15 @@ cd generated\layer-src
 mkdir  core_validation device_limits image object_tracker parameter_validation swapchain threading unique_objects
 cd ..\..
 xcopy /s gradle-templates\*   generated\gradle-build\
-
-copy ..\layers\core_validation.cpp   generated\layer-src\core_validation
-echo apply from: "../common.gradle"  > generated\gradle-build\core_validation\build.gradle
-copy ..\layers\device_limits.cpp   generated\layer-src\device_limits
-echo apply from: "../common.gradle"  > generated\gradle-build\device_limits\build.gradle
-copy ..\layers\image.cpp   generated\layer-src\image
-echo apply from: "../common.gradle"  > generated\gradle-build\image\build.gradle
-copy ..\layers\parameter_validation.cpp   generated\layer-src\parameter_validation
-echo apply from: "../common.gradle"  > generated\gradle-build\parameter_validation\build.gradle
-copy ..\layers\swapchain.cpp   generated\layer-src\swapchain
-echo apply from: "../common.gradle"  > generated\gradle-build\swapchain\build.gradle
-copy ..\layers\threading.cpp   generated\layer-src\threading
-echo apply from: "../common.gradle"  > generated\gradle-build\threading\build.gradle
+for %%G in (core_validation device_limits image parameter_validation swapchain threading) Do (
+    copy ..\layers\%%G.cpp   generated\layer-src\%%G
+    echo apply from: "../win.template.gradle"  > generated\gradle-build\%%G\build.gradle
+)
 copy generated\include\object_tracker.cpp   generated\layer-src\object_tracker
-echo apply from: "../common.gradle"  > generated\gradle-build\object_tracker\build.gradle
+echo apply from: "../win.template.gradle"  > generated\gradle-build\object_tracker\build.gradle
 copy generated\include\unique_objects.cpp   generated\layer-src\unique_objects
 move generated\include\vk_safe_struct.cpp generated\layer-src\unique_objects\vk_safe_struct.cpp
-echo apply from: "../common.gradle"  > generated\gradle-build\unique_objects\build.gradle
+echo apply from: "../win.template.gradle"  > generated\gradle-build\unique_objects\build.gradle
 
 del  /f /q generated\include\object_tracker.cpp
 del  /f /q generated\include\unique_objects.cpp
