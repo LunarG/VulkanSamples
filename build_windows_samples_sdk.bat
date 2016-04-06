560c09bb928b5fe2f3980f4a69f78d557a9e1eb7
Tony Barbour <tony@LunarG.com>
2016-04-06 14:39:08 -0600

    Build both debug and release in build_windows_samples_sdk.bat
    Change-Id: Iee8ab42537db4f80d649f3f5b49dcf39416b42f5

--- build_windows_samples_sdk.bat
+++ build_windows_samples_sdk.bat
@@ -4,16 +4,19 @@ md build
 cd build
 cmake -G "Visual Studio 12 Win64" ..
 msbuild ALL_BUILD.vcxproj /p:Platform=x64 /p:Configuration=Debug /verbosity:quiet
+msbuild ALL_BUILD.vcxproj /p:Platform=x64 /p:Configuration=Release /verbosity:quiet
 cd ..\..
 cd spirv-tools
 md build
 cd build
 cmake -G "Visual Studio 12 Win64" ..
 msbuild ALL_BUILD.vcxproj /p:Platform=x64 /p:Configuration=Debug /verbosity:quiet
+msbuild ALL_BUILD.vcxproj /p:Platform=x64 /p:Configuration=Release /verbosity:quiet
 cd ..\..
 cd samples
 md build
 cd build
 cmake -G "Visual Studio 12 Win64" ..
 msbuild ALL_BUILD.vcxproj /p:Platform=x64 /p:Configuration=Debug /verbosity:quiet
+msbuild ALL_BUILD.vcxproj /p:Platform=x64 /p:Configuration=Release /verbosity:quiet
 
