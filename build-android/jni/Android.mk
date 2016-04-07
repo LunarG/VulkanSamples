02d72703d40bfec6e5077cdbfbe59e419caf4da1
Cody Northrop <cnorthrop@google.com>
2016-04-07 11:54:50 -0600

    android: Clean up Android.mk

--- build-android/jni/Android.mk
+++ build-android/jni/Android.mk
@@ -14,7 +14,6 @@
 # limitations under the License.
 
 LOCAL_PATH := $(abspath $(call my-dir))
-MY_PATH := $(LOCAL_PATH)
 SRC_DIR := $(LOCAL_PATH)/../../
 LAYER_DIR := $(LOCAL_PATH)/../generated
 
@@ -24,7 +23,7 @@ LOCAL_SRC_FILES += $(LAYER_DIR)/common/vk_layer_config.cpp
 LOCAL_SRC_FILES += $(LAYER_DIR)/common/vk_layer_extension_utils.cpp
 LOCAL_SRC_FILES += $(LAYER_DIR)/common/vk_layer_utils.cpp
 LOCAL_C_INCLUDES += $(SRC_DIR)/include \
-                    ${SRC_DIR}/layers \
+                    $(SRC_DIR)/layers \
                     $(SRC_DIR)/loader
 LOCAL_CPPFLAGS += -DVK_USE_PLATFORM_ANDROID_KHR
 include $(BUILD_STATIC_LIBRARY)
@@ -35,8 +34,8 @@ LOCAL_SRC_FILES += $(LAYER_DIR)/layer-src/core_validation/core_validation.cpp
 LOCAL_SRC_FILES += $(LAYER_DIR)/common/vk_layer_table.cpp
 LOCAL_SRC_FILES += $(LAYER_DIR)/layer-src/unique_objects/vk_safe_struct.cpp
 LOCAL_C_INCLUDES += $(SRC_DIR)/include \
-                    ${SRC_DIR}/layers \
-                    $(MY_PATH)/../generated/include \
+                    $(SRC_DIR)/layers \
+                    $(LAYER_DIR)/include \
                     $(SRC_DIR)/loader \
                     $(SRC_DIR)/../glslang
 LOCAL_STATIC_LIBRARIES += layer_utils
@@ -49,8 +48,8 @@ LOCAL_MODULE := VkLayer_device_limits
 LOCAL_SRC_FILES += $(LAYER_DIR)/layer-src/device_limits/device_limits.cpp
 LOCAL_SRC_FILES += $(LAYER_DIR)/common/vk_layer_table.cpp
 LOCAL_C_INCLUDES += $(SRC_DIR)/include \
-                    ${SRC_DIR}/layers \
-                    $(MY_PATH)/../generated/include \
+                    $(SRC_DIR)/layers \
+                    $(LAYER_DIR)/include \
                     $(SRC_DIR)/loader
 LOCAL_STATIC_LIBRARIES += layer_utils
 LOCAL_CPPFLAGS += -DVK_USE_PLATFORM_ANDROID_KHR
@@ -62,8 +61,8 @@ LOCAL_MODULE := VkLayer_image
 LOCAL_SRC_FILES += $(LAYER_DIR)/layer-src/image/image.cpp
 LOCAL_SRC_FILES += $(LAYER_DIR)/common/vk_layer_table.cpp
 LOCAL_C_INCLUDES += $(SRC_DIR)/include \
-                    ${SRC_DIR}/layers \
-                    $(MY_PATH)/../generated/include \
+                    $(SRC_DIR)/layers \
+                    $(LAYER_DIR)/include \
                     $(SRC_DIR)/loader
 LOCAL_STATIC_LIBRARIES += layer_utils
 LOCAL_CPPFLAGS += -DVK_USE_PLATFORM_ANDROID_KHR
@@ -75,7 +74,7 @@ LOCAL_MODULE := VkLayer_parameter_validation
 LOCAL_SRC_FILES += $(LAYER_DIR)/layer-src/parameter_validation/parameter_validation.cpp
 LOCAL_SRC_FILES += $(LAYER_DIR)/common/vk_layer_table.cpp
 LOCAL_C_INCLUDES += $(SRC_DIR)/include \
-                    $(MY_PATH)/../generated/include \
+                    $(LAYER_DIR)/include \
                     $(SRC_DIR)/layers \
                     $(SRC_DIR)/loader
 LOCAL_STATIC_LIBRARIES += layer_utils
@@ -89,7 +88,7 @@ LOCAL_SRC_FILES += $(LAYER_DIR)/layer-src/object_tracker/object_tracker.cpp
 LOCAL_SRC_FILES += $(LAYER_DIR)/common/vk_layer_table.cpp
 LOCAL_C_INCLUDES += $(SRC_DIR)/include \
                     $(SRC_DIR)/layers \
-                    $(MY_PATH)/../generated/include \
+                    $(LAYER_DIR)/include \
                     $(SRC_DIR)/loader
 LOCAL_STATIC_LIBRARIES += layer_utils
 LOCAL_CPPFLAGS += -DVK_USE_PLATFORM_ANDROID_KHR
@@ -102,7 +101,7 @@ LOCAL_SRC_FILES += $(LAYER_DIR)/layer-src/threading/threading.cpp
 LOCAL_SRC_FILES += $(LAYER_DIR)/common/vk_layer_table.cpp
 LOCAL_C_INCLUDES += $(SRC_DIR)/include \
                     $(SRC_DIR)/layers \
-                    $(MY_PATH)/../generated/include \
+                    $(LAYER_DIR)/include \
                     $(SRC_DIR)/loader
 LOCAL_STATIC_LIBRARIES += layer_utils
 LOCAL_CPPFLAGS += -DVK_USE_PLATFORM_ANDROID_KHR
@@ -116,7 +115,7 @@ LOCAL_SRC_FILES += $(LAYER_DIR)/layer-src/unique_objects/vk_safe_struct.cpp
 LOCAL_SRC_FILES += $(LAYER_DIR)/common/vk_layer_table.cpp
 LOCAL_C_INCLUDES += $(SRC_DIR)/include \
                     $(SRC_DIR)/layers \
-                    $(MY_PATH)/../generated/include \
+                    $(LAYER_DIR)/include \
                     $(SRC_DIR)/loader
 LOCAL_STATIC_LIBRARIES += layer_utils
 LOCAL_CPPFLAGS += -DVK_USE_PLATFORM_ANDROID_KHR
@@ -128,8 +127,8 @@ LOCAL_MODULE := VkLayer_swapchain
 LOCAL_SRC_FILES += $(LAYER_DIR)/layer-src/swapchain/swapchain.cpp
 LOCAL_SRC_FILES += $(LAYER_DIR)/common/vk_layer_table.cpp
 LOCAL_C_INCLUDES += $(SRC_DIR)/include \
-                    ${SRC_DIR}/layers \
-                    $(MY_PATH)/../generated/include \
+                    $(SRC_DIR)/layers \
+                    $(LAYER_DIR)/include \
                     $(SRC_DIR)/loader
 LOCAL_STATIC_LIBRARIES += layer_utils
 LOCAL_CPPFLAGS += -DVK_USE_PLATFORM_ANDROID_KHR
