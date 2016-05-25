e1d60e04ca4c58b5333d09dacd68c3529319c7d8
Cody Northrop <cnorthrop@google.com>
2016-05-25 01:38:50 -0600

    windows: Return exit status from test script

--- tests/_run_all_tests.ps1
+++ tests/_run_all_tests.ps1
@@ -13,4 +13,8 @@ Set-Item -path env:Path -value ($env:Path + ";gtest-1.7.0\$dPath")
 $env:VK_LAYER_PATH = "..\layers\$dPath"
 
 & $dPath\vk_layer_validation_tests
+if ($lastexitcode -ne 0) {
+   exit 1
+}
 .\vkvalidatelayerdoc.ps1
+exit $lastexitcode
