191e9c9e5679c0ac181619c2baa21e655849b420
Jeremy Hayes <jeremy@lunarg.com>
2016-06-15 16:37:17 -0600

    tests: test layer and ext not present
    Change-Id: Ibf6314a5362423af47e655afc34df0da011b7483

--- tests/loader_validation_tests.cpp
+++ tests/loader_validation_tests.cpp
@@ -64,6 +64,126 @@ TEST(LX475, DestroyDeviceNullHandle)
     vkDestroyDevice(VK_NULL_HANDLE, nullptr);
 }
 
+TEST(CreateInstance, ExtensionNotPresent)
+{
+    char const*const names[] = {"NotPresent"}; // Temporary required due to Microsoft compiler bug.
+    VkInstanceCreateInfo const info =
+    {
+        VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO, // sType
+        nullptr, // pNext
+        0, // flags
+        nullptr, // pApplicationInfo
+        0, // enabledLayerCount
+        nullptr, // ppEnabledLayerNames
+        1, //enabledExtensionCount
+        names // ppEnabledExtensionNames
+    };
+
+    VkInstance instance = VK_NULL_HANDLE;
+    VkResult result = vkCreateInstance(&info, VK_NULL_HANDLE, &instance);
+    ASSERT_EQ(result, VK_ERROR_EXTENSION_NOT_PRESENT);
+}
+
+TEST(CreateInstance, LayerNotPresent)
+{
+    char const*const names[] = {"NotPresent"}; // Temporary required due to Microsoft compiler bug.
+    VkInstanceCreateInfo const info =
+    {
+        VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO, // sType
+        nullptr, // pNext
+        0, // flags
+        nullptr, // pApplicationInfo
+        1, // enabledLayerCount
+        names, // ppEnabledLayerNames
+        0, //enabledExtensionCount
+        nullptr // ppEnabledExtensionNames
+    };
+
+    VkInstance instance = VK_NULL_HANDLE;
+    VkResult result = vkCreateInstance(&info, VK_NULL_HANDLE, &instance);
+    ASSERT_EQ(result, VK_ERROR_LAYER_NOT_PRESENT);
+}
+
+TEST(CreateDevice, ExtensionNotPresent)
+{
+    VkInstanceCreateInfo const instanceInfo =
+    {
+        VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO, // sType
+        nullptr, // pNext
+        0, // flags
+        nullptr, // pApplicationInfo
+        0, // enabledLayerCount
+        nullptr, // ppEnabledLayerNames
+        0, //enabledExtensionCount
+        nullptr // ppEnabledExtensionNames
+    };
+
+    VkInstance instance = VK_NULL_HANDLE;
+    VkResult result = vkCreateInstance(&instanceInfo, VK_NULL_HANDLE, &instance);
+    ASSERT_EQ(result, VK_SUCCESS);
+
+    uint32_t physicalCount = 0;
+    result = vkEnumeratePhysicalDevices(instance, &physicalCount, nullptr);
+    ASSERT_EQ(result, VK_SUCCESS);
+    ASSERT_GT(physicalCount, 0);
+
+    std::unique_ptr<VkPhysicalDevice[]> physical(new VkPhysicalDevice[physicalCount]);
+    result = vkEnumeratePhysicalDevices(instance, &physicalCount, physical.get());
+    ASSERT_EQ(result, VK_SUCCESS);
+    ASSERT_GT(physicalCount, 0);
+
+    for(uint32_t p = 0; p < physicalCount; ++p)
+    {
+        uint32_t familyCount = 0;
+        vkGetPhysicalDeviceQueueFamilyProperties(physical[p], &familyCount, nullptr);
+        ASSERT_EQ(result, VK_SUCCESS);
+        ASSERT_GT(familyCount, 0);
+
+        std::unique_ptr<VkQueueFamilyProperties[]> family(new VkQueueFamilyProperties[familyCount]);
+        vkGetPhysicalDeviceQueueFamilyProperties(physical[p], &familyCount, family.get());
+        ASSERT_EQ(result, VK_SUCCESS);
+        ASSERT_GT(familyCount, 0);
+
+        for(uint32_t q = 0; q < familyCount; ++q)
+        {
+            if(~family[q].queueFlags & VK_QUEUE_GRAPHICS_BIT)
+            {
+                continue;
+            }
+
+            float const priorities[] = {0.0f}; // Temporary required due to Microsoft compiler bug.
+            VkDeviceQueueCreateInfo queueInfo[1] =
+            {
+                VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO, // sType
+                nullptr, // pNext
+                0, // flags
+                q, // queueFamilyIndex
+                1, // queueCount
+                priorities // priorities
+            };
+
+            char const*const names[] = {"NotPresent"}; // Temporary required due to Microsoft compiler bug.
+            VkDeviceCreateInfo const deviceInfo =
+            {
+                VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO, // sType
+                nullptr, // pNext
+                0, // flags
+                1, // queueCreateInfoCount
+                queueInfo, // pQueueCreateInfos
+                0, // enabledLayerCount
+                nullptr, // ppEnabledLayerNames
+                1, //enabledExtensionCount
+                names, // ppEnabledExtensionNames
+                nullptr // pEnabledFeatures
+            };
+
+            VkDevice device;
+            result = vkCreateDevice(physical[p], &deviceInfo, nullptr, &device);
+            ASSERT_EQ(result, VK_ERROR_EXTENSION_NOT_PRESENT);
+        }
+    }
+}
+
 int main(int argc, char **argv)
 {
     int result;
