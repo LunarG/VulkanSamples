fb5c9d8c267491ac8cea0b285485112e4a6a22ce
Jeremy Hayes <jeremy@lunarg.com>
2016-06-28 11:29:05 -0600

    tests: test layer insertion
    Change-Id: I315a7312514fba817906facf42597c858e2fecd9

--- tests/loader_validation_tests.cpp
+++ tests/loader_validation_tests.cpp
@@ -30,6 +30,273 @@
 #include <vulkan/vulkan.h>
 #include "test_common.h"
 
+namespace VK
+{
+
+struct InstanceCreateInfo
+{
+    InstanceCreateInfo() :
+        info // MSVC can't handle list initialization, thus explicit construction herein.
+        (
+            VkInstanceCreateInfo
+            {
+                VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO, // sType
+                nullptr, // pNext
+                0, // flags
+                nullptr, // pApplicationInfo
+                0, // enabledLayerCount
+                nullptr, // ppEnabledLayerNames
+                0, //enabledExtensionCount
+                nullptr // ppEnabledExtensionNames
+            }
+        )
+    {
+    }
+
+    InstanceCreateInfo& sType(VkStructureType const& sType)
+    {
+        info.sType = sType;
+
+        return *this;
+    }
+
+    InstanceCreateInfo& pNext(void const*const pNext)
+    {
+        info.pNext = pNext;
+
+        return *this;
+    }
+
+    InstanceCreateInfo& flags(VkInstanceCreateFlags const& flags)
+    {
+        info.flags = flags;
+
+        return *this;
+    }
+
+    InstanceCreateInfo& pApplicationInfo(VkApplicationInfo const*const pApplicationInfo)
+    {
+        info.pApplicationInfo = pApplicationInfo;
+
+        return *this;
+    }
+
+    InstanceCreateInfo& enabledLayerCount(uint32_t const& enabledLayerCount)
+    {
+        info.enabledLayerCount = enabledLayerCount;
+
+        return *this;
+    }
+
+    InstanceCreateInfo& ppEnabledLayerNames(char const*const*const ppEnabledLayerNames)
+    {
+        info.ppEnabledLayerNames = ppEnabledLayerNames;
+
+        return *this;
+    }
+
+    InstanceCreateInfo& enabledExtensionCount(uint32_t const& enabledExtensionCount)
+    {
+        info.enabledExtensionCount = enabledExtensionCount;
+
+        return *this;
+    }
+
+    InstanceCreateInfo& ppEnabledExtensionNames(char const*const*const ppEnabledExtensionNames)
+    {
+        info.ppEnabledExtensionNames = ppEnabledExtensionNames;
+
+        return *this;
+    }
+
+    operator VkInstanceCreateInfo const*() const
+    {
+        return &info;
+    }
+
+    operator VkInstanceCreateInfo*()
+    {
+        return &info;
+    }
+
+    VkInstanceCreateInfo info;
+};
+
+struct DeviceQueueCreateInfo
+{
+    DeviceQueueCreateInfo() :
+        info // MSVC can't handle list initialization, thus explicit construction herein.
+        (
+            VkDeviceQueueCreateInfo
+            {
+                VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO, // sType
+                nullptr, // pNext
+                0, // flags
+                0, // queueFamilyIndex
+                0, // queueCount
+                nullptr // pQueuePriorities
+            }
+        )
+    {
+    }
+
+    DeviceQueueCreateInfo& sType(VkStructureType const& sType)
+    {
+        info.sType = sType;
+
+        return *this;
+    }
+
+    DeviceQueueCreateInfo& pNext(void const*const pNext)
+    {
+        info.pNext = pNext;
+
+        return *this;
+    }
+
+    DeviceQueueCreateInfo& flags(VkDeviceQueueCreateFlags const& flags)
+    {
+        info.flags = flags;
+
+        return *this;
+    }
+
+    DeviceQueueCreateInfo& queueFamilyIndex(uint32_t const& queueFamilyIndex)
+    {
+        info.queueFamilyIndex = queueFamilyIndex;
+
+        return *this;
+    }
+
+    DeviceQueueCreateInfo& queueCount(uint32_t const& queueCount)
+    {
+        info.queueCount = queueCount;
+
+        return *this;
+    }
+
+    DeviceQueueCreateInfo& pQueuePriorities(float const*const pQueuePriorities)
+    {
+        info.pQueuePriorities = pQueuePriorities;
+
+        return *this;
+    }
+
+    operator VkDeviceQueueCreateInfo()
+    {
+        return info;
+    }
+
+    VkDeviceQueueCreateInfo info;
+};
+
+struct DeviceCreateInfo
+{
+    DeviceCreateInfo() :
+        info // MSVC can't handle list initialization, thus explicit construction herein.
+        (
+            VkDeviceCreateInfo
+            {
+                VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO, // sType
+                nullptr, // pNext
+                0, // flags
+                0, // queueCreateInfoCount
+                nullptr, // pQueueCreateInfos
+                0, // enabledLayerCount
+                nullptr, // ppEnabledLayerNames
+                0, // enabledExtensionCount
+                nullptr, // ppEnabledExtensionNames
+                nullptr // pEnabledFeatures
+            }
+        )
+    {
+    }
+
+    DeviceCreateInfo& sType(VkStructureType const& sType)
+    {
+        info.sType = sType;
+
+        return *this;
+    }
+
+    DeviceCreateInfo& pNext(void const*const pNext)
+    {
+        info.pNext = pNext;
+
+        return *this;
+    }
+
+    DeviceCreateInfo& flags(VkDeviceQueueCreateFlags const& flags)
+    {
+        info.flags = flags;
+
+        return *this;
+    }
+
+    DeviceCreateInfo& queueCreateInfoCount(uint32_t const& queueCreateInfoCount)
+    {
+        info.queueCreateInfoCount = queueCreateInfoCount;
+
+        return *this;
+    }
+
+    DeviceCreateInfo& pQueueCreateInfos(VkDeviceQueueCreateInfo const*const pQueueCreateInfos)
+    {
+        info.pQueueCreateInfos = pQueueCreateInfos;
+
+        return *this;
+    }
+
+    DeviceCreateInfo& enabledLayerCount(uint32_t const& enabledLayerCount)
+    {
+        info.enabledLayerCount = enabledLayerCount;
+
+        return *this;
+    }
+
+    DeviceCreateInfo& ppEnabledLayerNames(char const*const*const ppEnabledLayerNames)
+    {
+        info.ppEnabledLayerNames = ppEnabledLayerNames;
+
+        return *this;
+    }
+
+    DeviceCreateInfo& enabledExtensionCount(uint32_t const& enabledExtensionCount)
+    {
+        info.enabledExtensionCount = enabledExtensionCount;
+
+        return *this;
+    }
+
+    DeviceCreateInfo& ppEnabledExtensionNames(char const*const*const ppEnabledExtensionNames)
+    {
+        info.ppEnabledExtensionNames = ppEnabledExtensionNames;
+
+        return *this;
+    }
+
+    DeviceCreateInfo& pEnabledFeatures(VkPhysicalDeviceFeatures const*const pEnabledFeatures)
+    {
+        info.pEnabledFeatures = pEnabledFeatures;
+
+        return *this;
+    }
+
+    operator VkDeviceCreateInfo const*() const
+    {
+        return &info;
+    }
+
+    operator VkDeviceCreateInfo*()
+    {
+        return &info;
+    }
+
+    VkDeviceCreateInfo info;
+};
+
+}
+
 // Test groups:
 // LX = lunar exchange
 // LVLGH = loader and validation github
@@ -37,7 +304,7 @@
 
 TEST(LX435, InstanceCreateInfoConst)
 {
-    const VkInstanceCreateInfo info =
+    VkInstanceCreateInfo const info =
     {
         VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
         nullptr,
@@ -66,60 +333,32 @@ TEST(LX475, DestroyDeviceNullHandle)
 
 TEST(CreateInstance, ExtensionNotPresent)
 {
-    char const*const names[] = {"NotPresent"}; // Temporary required due to Microsoft compiler bug.
-    VkInstanceCreateInfo const info =
-    {
-        VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO, // sType
-        nullptr, // pNext
-        0, // flags
-        nullptr, // pApplicationInfo
-        0, // enabledLayerCount
-        nullptr, // ppEnabledLayerNames
-        1, //enabledExtensionCount
-        names // ppEnabledExtensionNames
-    };
+    char const*const names[] = {"NotPresent"}; // Temporary required due to MSVC bug.
+    auto const info = VK::InstanceCreateInfo().
+        enabledExtensionCount(1).
+        ppEnabledExtensionNames(names);
 
     VkInstance instance = VK_NULL_HANDLE;
-    VkResult result = vkCreateInstance(&info, VK_NULL_HANDLE, &instance);
+    VkResult result = vkCreateInstance(info, VK_NULL_HANDLE, &instance);
     ASSERT_EQ(result, VK_ERROR_EXTENSION_NOT_PRESENT);
 }
 
 TEST(CreateInstance, LayerNotPresent)
 {
-    char const*const names[] = {"NotPresent"}; // Temporary required due to Microsoft compiler bug.
-    VkInstanceCreateInfo const info =
-    {
-        VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO, // sType
-        nullptr, // pNext
-        0, // flags
-        nullptr, // pApplicationInfo
-        1, // enabledLayerCount
-        names, // ppEnabledLayerNames
-        0, //enabledExtensionCount
-        nullptr // ppEnabledExtensionNames
-    };
+    char const*const names[] = {"NotPresent"}; // Temporary required due to MSVC bug.
+    auto const info = VK::InstanceCreateInfo().
+        enabledLayerCount(1).
+        ppEnabledLayerNames(names);
 
     VkInstance instance = VK_NULL_HANDLE;
-    VkResult result = vkCreateInstance(&info, VK_NULL_HANDLE, &instance);
+    VkResult result = vkCreateInstance(info, VK_NULL_HANDLE, &instance);
     ASSERT_EQ(result, VK_ERROR_LAYER_NOT_PRESENT);
 }
 
 TEST(CreateDevice, ExtensionNotPresent)
 {
-    VkInstanceCreateInfo const instanceInfo =
-    {
-        VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO, // sType
-        nullptr, // pNext
-        0, // flags
-        nullptr, // pApplicationInfo
-        0, // enabledLayerCount
-        nullptr, // ppEnabledLayerNames
-        0, //enabledExtensionCount
-        nullptr // ppEnabledExtensionNames
-    };
-
     VkInstance instance = VK_NULL_HANDLE;
-    VkResult result = vkCreateInstance(&instanceInfo, VK_NULL_HANDLE, &instance);
+    VkResult result = vkCreateInstance(VK::InstanceCreateInfo(), VK_NULL_HANDLE, &instance);
     ASSERT_EQ(result, VK_SUCCESS);
 
     uint32_t physicalCount = 0;
@@ -151,34 +390,24 @@ TEST(CreateDevice, ExtensionNotPresent)
                 continue;
             }
 
-            float const priorities[] = {0.0f}; // Temporary required due to Microsoft compiler bug.
-            VkDeviceQueueCreateInfo queueInfo[1] =
+            float const priorities[] = {0.0f}; // Temporary required due to MSVC bug.
+            VkDeviceQueueCreateInfo const queueInfo[1]
             {
-                VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO, // sType
-                nullptr, // pNext
-                0, // flags
-                q, // queueFamilyIndex
-                1, // queueCount
-                priorities // priorities
+                VK::DeviceQueueCreateInfo().
+                    queueFamilyIndex(q).
+                    queueCount(1).
+                    pQueuePriorities(priorities)
             };
 
-            char const*const names[] = {"NotPresent"}; // Temporary required due to Microsoft compiler bug.
-            VkDeviceCreateInfo const deviceInfo =
-            {
-                VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO, // sType
-                nullptr, // pNext
-                0, // flags
-                1, // queueCreateInfoCount
-                queueInfo, // pQueueCreateInfos
-                0, // enabledLayerCount
-                nullptr, // ppEnabledLayerNames
-                1, //enabledExtensionCount
-                names, // ppEnabledExtensionNames
-                nullptr // pEnabledFeatures
-            };
+            char const*const names[] = {"NotPresent"}; // Temporary required due to MSVC bug.
+            auto const deviceInfo = VK::DeviceCreateInfo().
+                queueCreateInfoCount(1).
+                pQueueCreateInfos(queueInfo).
+                enabledExtensionCount(1).
+                ppEnabledExtensionNames(names);
 
             VkDevice device;
-            result = vkCreateDevice(physical[p], &deviceInfo, nullptr, &device);
+            result = vkCreateDevice(physical[p], deviceInfo, nullptr, &device);
             ASSERT_EQ(result, VK_ERROR_EXTENSION_NOT_PRESENT);
         }
     }
@@ -187,22 +416,10 @@ TEST(CreateDevice, ExtensionNotPresent)
 // LX535 / MI-76: Device layers are deprecated.
 // For backwards compatibility, they are allowed, but must be ignored.
 // Ensure that no errors occur if a bogus device layer list is passed to vkCreateDevice.
-TEST(CreateDevice, LX535_IgnoreDeviceLayers)
+TEST(CreateDevice, LayersNotPresent)
 {
-    VkInstanceCreateInfo const instanceInfo =
-    {
-        VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO, // sType
-        nullptr, // pNext
-        0,       // flags
-        nullptr, // pApplicationInfo
-        0,       // enabledLayerCount
-        nullptr, // ppEnabledLayerNames
-        0,       // enabledExtensionCount
-        nullptr  // ppEnabledExtensionNames
-    };
-
     VkInstance instance = VK_NULL_HANDLE;
-    VkResult result = vkCreateInstance(&instanceInfo, VK_NULL_HANDLE, &instance);
+    VkResult result = vkCreateInstance(VK::InstanceCreateInfo(), VK_NULL_HANDLE, &instance);
     ASSERT_EQ(result, VK_SUCCESS);
 
     uint32_t physicalCount = 0;
@@ -234,35 +451,79 @@ TEST(CreateDevice, LX535_IgnoreDeviceLayers)
                 continue;
             }
 
-            float const priorities[] = {0.0f}; // Temporary required due to Microsoft compiler bug.
-            VkDeviceQueueCreateInfo queueInfo[1] =
+            float const priorities[] = {0.0f}; // Temporary required due to MSVC bug.
+            VkDeviceQueueCreateInfo const queueInfo[1]
             {
-                VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO, // sType
-                nullptr,   // pNext
-                0,         // flags
-                q,         // queueFamilyIndex
-                1,         // queueCount
-                priorities // priorities
+                VK::DeviceQueueCreateInfo().
+                    queueFamilyIndex(q).
+                    queueCount(1).
+                    pQueuePriorities(priorities)
             };
 
-            const char* layer_names[] = {"LAYER1", "LAYER2"};  // bogus layer names
-
-            VkDeviceCreateInfo const deviceInfo =
-            {
-                VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO, // sType
-                nullptr,       // pNext
-                0,             // flags
-                1,             // queueCreateInfoCount
-                queueInfo,     // pQueueCreateInfos
-                2,             // enabledLayerCount
-                layer_names,   // ppEnabledLayerNames      (deprecated parameter... must be ignored)
-                0,             // enabledExtensionCount
-                nullptr,       // ppEnabledExtensionNames
-                nullptr        // pEnabledFeatures
-            };
+            char const*const names[] = {"NotPresent"}; // Temporary required due to MSVC bug.
+            auto const deviceInfo = VK::DeviceCreateInfo().
+                queueCreateInfoCount(1).
+                pQueueCreateInfos(queueInfo).
+                enabledLayerCount(1).
+                ppEnabledLayerNames(names);
 
             VkDevice device;
-            result = vkCreateDevice(physical[p], &deviceInfo, nullptr, &device);
+            result = vkCreateDevice(physical[p], deviceInfo, nullptr, &device);
+            ASSERT_EQ(result, VK_SUCCESS);
+        }
+    }
+}
+
+TEST(WrapObjects, Insert)
+{
+    VkInstance instance = VK_NULL_HANDLE;
+    VkResult result = vkCreateInstance(VK::InstanceCreateInfo(), VK_NULL_HANDLE, &instance);
+    ASSERT_EQ(result, VK_SUCCESS);
+
+    uint32_t physicalCount = 0;
+    result = vkEnumeratePhysicalDevices(instance, &physicalCount, nullptr);
+    ASSERT_EQ(result, VK_SUCCESS);
+    ASSERT_GT(physicalCount, 0u);
+
+    std::unique_ptr<VkPhysicalDevice[]> physical(new VkPhysicalDevice[physicalCount]);
+    result = vkEnumeratePhysicalDevices(instance, &physicalCount, physical.get());
+    ASSERT_EQ(result, VK_SUCCESS);
+    ASSERT_GT(physicalCount, 0u);
+
+    for(uint32_t p = 0; p < physicalCount; ++p)
+    {
+        uint32_t familyCount = 0;
+        vkGetPhysicalDeviceQueueFamilyProperties(physical[p], &familyCount, nullptr);
+        ASSERT_EQ(result, VK_SUCCESS);
+        ASSERT_GT(familyCount, 0u);
+
+        std::unique_ptr<VkQueueFamilyProperties[]> family(new VkQueueFamilyProperties[familyCount]);
+        vkGetPhysicalDeviceQueueFamilyProperties(physical[p], &familyCount, family.get());
+        ASSERT_EQ(result, VK_SUCCESS);
+        ASSERT_GT(familyCount, 0u);
+
+        for(uint32_t q = 0; q < familyCount; ++q)
+        {
+            if(~family[q].queueFlags & VK_QUEUE_GRAPHICS_BIT)
+            {
+                continue;
+            }
+
+            float const priorities[] = {0.0f}; // Temporary required due to MSVC bug.
+            VkDeviceQueueCreateInfo const queueInfo[1]
+            {
+                VK::DeviceQueueCreateInfo().
+                    queueFamilyIndex(q).
+                    queueCount(1).
+                    pQueuePriorities(priorities)
+            };
+
+            auto const deviceInfo = VK::DeviceCreateInfo().
+                queueCreateInfoCount(1).
+                pQueueCreateInfos(queueInfo);
+
+            VkDevice device;
+            result = vkCreateDevice(physical[p], deviceInfo, nullptr, &device);
             ASSERT_EQ(result, VK_SUCCESS);
         }
     }
--- tests/test_wrap_objects.sh
+++ tests/test_wrap_objects.sh
@@ -4,27 +4,134 @@ pushd $(dirname "$0") > /dev/null
 
 echo "Testing wrap-objects layer..."
 
-# Don't print the elapsed time of each test.
-export GTEST_PRINT_TIME=0
-
-# Run the layer validation tests with and without the wrap-objects layer. Diff the results.
-diff \
-   <(./vk_layer_validation_tests) \
-   <(VK_LAYER_PATH=$VK_LAYER_PATH:`pwd`/layers \
+# Check for insertion of wrap-objects layer.
+output=$(VK_LAYER_PATH=$VK_LAYER_PATH:`pwd`/layers \
    LD_LIBRARY_PATH=$LD_LIBRARY_PATH:`pwd`/layers \
    VK_INSTANCE_LAYERS=VK_LAYER_LUNARG_wrap_objects \
-   ./vk_layer_validation_tests) \
+   VK_LOADER_DEBUG=all \
+   GTEST_FILTER=WrapObjects.Insert \
+   ./vk_loader_validation_tests 2>&1)
 
-result=$?
+echo "$output" | grep -q "Insert instance layer VK_LAYER_LUNARG_wrap_objects"
+ec=$?
 
-popd > /dev/null
-
-if [ $result -eq 1 ]
+if [ $ec -eq 1 ]
 then
-   echo "TEST FAILED - Diff detected" >&2
+   echo "Insertion test FAILED - wrap-objects not detected in instance layers" >&2
    exit 1
 fi
 
+echo "$output" | grep -q "Insert device layer VK_LAYER_LUNARG_wrap_objects"
+ec=$?
+
+if [ $ec -eq 1 ]
+then
+   echo "Insertion test FAILED - wrap-objects not detected in device layers" >&2
+   exit 1
+fi
+echo "Insertion test PASSED"
+
+# Check for insertion of wrap-objects layer in front.
+output=$(VK_LAYER_PATH=$VK_LAYER_PATH:`pwd`/layers \
+   LD_LIBRARY_PATH=$LD_LIBRARY_PATH:`pwd`/layers \
+   VK_INSTANCE_LAYERS=VK_LAYER_LUNARG_parameter_validation:VK_LAYER_LUNARG_wrap_objects \
+   VK_LOADER_DEBUG=all \
+   GTEST_FILTER=WrapObjects.Insert \
+   ./vk_loader_validation_tests 2>&1)
+
+echo "$output" | grep -q "Insert instance layer VK_LAYER_LUNARG_wrap_objects"
+ec=$?
+
+if [ $ec -eq 1 ]
+then
+   echo "Front insertion test FAILED - wrap-objects not detected in instance layers" >&2
+   exit 1
+fi
+
+echo "$output" | grep -q "Insert device layer VK_LAYER_LUNARG_wrap_objects"
+ec=$?
+
+if [ $ec -eq 1 ]
+then
+   echo "Front insertion test FAILED - wrap-objects not detected in device layers" >&2
+   exit 1
+fi
+echo "Front insertion test PASSED"
+
+# Check for insertion of wrap-objects layer in back.
+output=$(VK_LAYER_PATH=$VK_LAYER_PATH:`pwd`/layers \
+   LD_LIBRARY_PATH=$LD_LIBRARY_PATH:`pwd`/layers \
+   VK_INSTANCE_LAYERS=VK_LAYER_LUNARG_wrap_objects:VK_LAYER_LUNARG_parameter_validation \
+   VK_LOADER_DEBUG=all \
+   GTEST_FILTER=WrapObjects.Insert \
+   ./vk_loader_validation_tests 2>&1)
+
+echo "$output" | grep -q "Insert instance layer VK_LAYER_LUNARG_wrap_objects"
+ec=$?
+
+if [ $ec -eq 1 ]
+then
+   echo "Back insertion test FAILED - wrap-objects not detected in instance layers" >&2
+   exit 1
+fi
+
+echo "$output" | grep -q "Insert device layer VK_LAYER_LUNARG_wrap_objects"
+ec=$?
+
+if [ $ec -eq 1 ]
+then
+   echo "Back insertion test FAILED - wrap-objects not detected in device layers" >&2
+   exit 1
+fi
+echo "Back insertion test PASSED"
+
+# Check for insertion of wrap-objects layer in middle.
+output=$(VK_LAYER_PATH=$VK_LAYER_PATH:`pwd`/layers \
+   LD_LIBRARY_PATH=$LD_LIBRARY_PATH:`pwd`/layers \
+   VK_INSTANCE_LAYERS=VK_LAYER_LUNARG_device_limits:VK_LAYER_LUNARG_wrap_objects:VK_LAYER_LUNARG_parameter_validation \
+   VK_LOADER_DEBUG=all \
+   GTEST_FILTER=WrapObjects.Insert \
+   ./vk_loader_validation_tests 2>&1)
+
+echo "$output" | grep -q "Insert instance layer VK_LAYER_LUNARG_wrap_objects"
+ec=$?
+
+if [ $ec -eq 1 ]
+then
+   echo "Middle insertion test FAILED - wrap-objects not detected in instance layers" >&2
+   exit 1
+fi
+
+echo "$output" | grep -q "Insert device layer VK_LAYER_LUNARG_wrap_objects"
+ec=$?
+
+if [ $ec -eq 1 ]
+then
+   echo "Middle insertion test FAILED - wrap-objects not detected in device layers" >&2
+   exit 1
+fi
+echo "Middle insertion test PASSED"
+
+# Run the layer validation tests with and without the wrap-objects layer. Diff the results.
+diff \
+   <(GTEST_PRINT_TIME=0 \
+      ./vk_layer_validation_tests) \
+   <(GTEST_PRINT_TIME=0 \
+      VK_LAYER_PATH=$VK_LAYER_PATH:`pwd`/layers \
+      LD_LIBRARY_PATH=$LD_LIBRARY_PATH:`pwd`/layers \
+      VK_INSTANCE_LAYERS=VK_LAYER_LUNARG_wrap_objects \
+      ./vk_layer_validation_tests)
+ec=$?
+
+if [ $ec -eq 1 ]
+then
+   echo "TEST FAILED - wrap-objects altered the results of the layer validation tests" >&2
+   exit 1
+fi
+echo "wrap-objects completed layer validation tests"
+
 echo "TEST PASSED"
+
+popd > /dev/null
 
 exit 0
