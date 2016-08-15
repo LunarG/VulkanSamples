9c5aff1f7a862d07c573741ae667ef1044548928
Mark Lobodzinski <mark@lunarg.com>
2016-08-15 16:21:08 -0600

    tests: Modify swapchain test for PV messages
    Parameter_validation now handles parameter and valid usage checks.
    Updated the SetDesiredMessage text for these cases.
    
    Change-Id: Ib3d228eae9b347163a9a347e04db752d421b1291

--- tests/layer_validation_tests.cpp
+++ tests/layer_validation_tests.cpp
@@ -2498,7 +2498,7 @@ TEST_F(VkWsiEnabledLayerTest, TestEnabledWsi) {
     // before successfully creating a surface:
     // First, try to create a surface without a VkXcbSurfaceCreateInfoKHR:
     m_errorMonitor->SetDesiredFailureMsg(VK_DEBUG_REPORT_ERROR_BIT_EXT,
-                                         "called with NULL pointer");
+                                         "required parameter pCreateInfo specified as NULL");
     err = vkCreateXcbSurfaceKHR(instance(), NULL, NULL, &surface);
     pass = (err != VK_SUCCESS);
     ASSERT_TRUE(pass);
@@ -2509,7 +2509,7 @@ TEST_F(VkWsiEnabledLayerTest, TestEnabledWsi) {
     VkXcbSurfaceCreateInfoKHR xcb_create_info = {};
     xcb_create_info.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
     m_errorMonitor->SetDesiredFailureMsg(VK_DEBUG_REPORT_ERROR_BIT_EXT,
-                                         "called with the wrong value for");
+                                         "parameter pCreateInfo->sType must be");
     err = vkCreateXcbSurfaceKHR(instance(), &xcb_create_info, NULL, &surface);
     pass = (err != VK_SUCCESS);
     ASSERT_TRUE(pass);
@@ -2640,7 +2640,8 @@ TEST_F(VkWsiEnabledLayerTest, TestEnabledWsi) {
 
     // First, try without a pointer to surface_format_count:
     m_errorMonitor->SetDesiredFailureMsg(VK_DEBUG_REPORT_ERROR_BIT_EXT,
-                                         "called with NULL pointer");
+                                         "required parameter pSurfaceFormatCount "
+                                         "specified as NULL");
     vkGetPhysicalDeviceSurfaceFormatsKHR(gpu(), surface, NULL, NULL);
     pass = (err == VK_SUCCESS);
     ASSERT_TRUE(pass);
@@ -2694,7 +2695,9 @@ TEST_F(VkWsiEnabledLayerTest, TestEnabledWsi) {
 
     // First, try without a pointer to surface_format_count:
     m_errorMonitor->SetDesiredFailureMsg(VK_DEBUG_REPORT_ERROR_BIT_EXT,
-                                         "called with NULL pointer");
+                                         "required parameter pPresentModeCount "
+                                         "specified as NULL");
+
     vkGetPhysicalDeviceSurfacePresentModesKHR(gpu(), surface, NULL, NULL);
     pass = (err == VK_SUCCESS);
     ASSERT_TRUE(pass);
@@ -2747,7 +2750,9 @@ TEST_F(VkWsiEnabledLayerTest, TestEnabledWsi) {
 
     // First, try without a pointer to swapchain_create_info:
     m_errorMonitor->SetDesiredFailureMsg(VK_DEBUG_REPORT_ERROR_BIT_EXT,
-                                         "called with NULL pointer");
+                                         "required parameter pCreateInfo "
+                                         "specified as NULL");
+
     err = vkCreateSwapchainKHR(m_device->device(), NULL, NULL, &swapchain);
     pass = (err != VK_SUCCESS);
     ASSERT_TRUE(pass);
@@ -2757,7 +2762,8 @@ TEST_F(VkWsiEnabledLayerTest, TestEnabledWsi) {
     // sType:
     swapchain_create_info.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
     m_errorMonitor->SetDesiredFailureMsg(VK_DEBUG_REPORT_ERROR_BIT_EXT,
-                                         "called with the wrong value for");
+                                         "parameter pCreateInfo->sType must be");
+
     err = vkCreateSwapchainKHR(m_device->device(), &swapchain_create_info, NULL,
                                &swapchain);
     pass = (err != VK_SUCCESS);
@@ -2769,7 +2775,9 @@ TEST_F(VkWsiEnabledLayerTest, TestEnabledWsi) {
     swapchain_create_info.pNext = NULL;
     swapchain_create_info.flags = 0;
     m_errorMonitor->SetDesiredFailureMsg(VK_DEBUG_REPORT_ERROR_BIT_EXT,
-                                         "called with NULL pointer");
+                                         "required parameter pSwapchain "
+                                         "specified as NULL");
+
     err = vkCreateSwapchainKHR(m_device->device(), &swapchain_create_info, NULL,
                                NULL);
     pass = (err != VK_SUCCESS);
