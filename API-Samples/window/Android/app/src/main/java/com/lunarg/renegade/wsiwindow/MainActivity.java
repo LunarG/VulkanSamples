72b7d27132a904bfbf6b99e28e20defb8fd19107
Rene Lindsay <rene@lunarg.com>
2016-08-30 10:33:03 -0600

    demos: WSIWindow Android
    demos: WSIWindow Android
    
    
    demos: WSIWindow Android
    
    
    demos: WSIWindow Android

---
+++ API-Samples/window/Android/app/src/main/java/com/lunarg/renegade/wsiwindow/MainActivity.java
@@ -0,0 +1,29 @@
+package com.lunarg.renegade.wsiwindow;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Example of a call to a native method
+        TextView tv = (TextView) findViewById(R.id.sample_text);
+        tv.setText(stringFromJNI());
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+}
