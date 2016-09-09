973c2aab1f20c53f219ce9f1e3002f1bdc01046c
Derrick Owens <derrick.owens@amd.com>
2016-09-09 15:49:07 -0400

    loader: Fix another possible memory leak
    Added another cleanup case, just in case...
    
    Change-Id: Ia6e09ce12c29314a1cce6b1cdcd72bdd7749f655

--- loader/loader.c
+++ loader/loader.c
@@ -2912,6 +2912,8 @@ VkResult loader_icd_scan(const struct loader_instance *inst,
             if (num_good_icds == 0) {
                 res = VK_ERROR_INITIALIZATION_FAILED;
             }
+            cJSON_Delete(json);
+            json = NULL;
             continue;
         }
         char *file_vers = cJSON_Print(item);
