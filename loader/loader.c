3a0201d7ad4eb2ad2e8b336e5030fd28d482b528
Derrick Owens <derrick.owens@amd.com>
2016-09-09 15:45:13 -0400

    loader: Fixed conformance memory leak.
    This fixes a memory leak found whilst running Khronos conformance with
    more than 1 ICD installed.
    
    Change-Id: I34ed978b2929c33d9e2c49cf633f09ada2788a92

--- loader/loader.c
+++ loader/loader.c
@@ -2921,6 +2921,8 @@ VkResult loader_icd_scan(const struct loader_instance *inst,
             if (num_good_icds == 0) {
                 res = VK_ERROR_OUT_OF_HOST_MEMORY;
             }
+            cJSON_Delete(json);
+            json = NULL;
             continue;
         }
         loader_log(inst, VK_DEBUG_REPORT_INFORMATION_BIT_EXT, 0,
