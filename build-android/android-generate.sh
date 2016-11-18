da10201f0c5cc12f4a6a4a95065314b06b13ef69
Mark Lobodzinski <mark@lunarg.com>
2016-11-18 15:04:50 -0700

    build: Move vk.xml file into scripts dir
    - Update android-generate batch/script files
    - Update layers CMakeLists.txt file
    
    Change-Id: I6423f441d61daa20aef2cc5e8bc719113fd7978d

--- build-android/android-generate.sh
+++ build-android/android-generate.sh
@@ -26,9 +26,9 @@ python ../scripts/vk-generate.py Android dispatch-table-ops layer > generated/in
 python ../scripts/vk_helper.py --gen_enum_string_helper ../include/vulkan/vulkan.h --abs_out_dir generated/include
 python ../scripts/vk_helper.py --gen_struct_wrappers ../include/vulkan/vulkan.h --abs_out_dir generated/include
 
-( cd generated/include; python ../../../scripts/lvl_genvk.py -registry ../../../vk.xml thread_check.h )
-( cd generated/include; python ../../../scripts/lvl_genvk.py -registry ../../../vk.xml parameter_validation.h )
-( cd generated/include; python ../../../scripts/lvl_genvk.py -registry ../../../vk.xml unique_objects_wrappers.h )
+( cd generated/include; python ../../../scripts/lvl_genvk.py -registry ../../../scripts/vk.xml thread_check.h )
+( cd generated/include; python ../../../scripts/lvl_genvk.py -registry ../../../scripts/vk.xml parameter_validation.h )
+( cd generated/include; python ../../../scripts/lvl_genvk.py -registry ../../../scripts/vk.xml unique_objects_wrappers.h )
 
 cp -f ../layers/vk_layer_config.cpp   generated/common/
 cp -f ../layers/vk_layer_extension_utils.cpp  generated/common/
