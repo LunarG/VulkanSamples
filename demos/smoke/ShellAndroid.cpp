6cbb622d83693fe2696fe11175703e90d893e85a
Cody Northrop <cnorthrop@google.com>
2016-12-07 01:58:34 -0800

    smoke: Don't load validation layers by default
    This was an inadvertent change from 3f3748ad

--- demos/smoke/ShellAndroid.cpp
+++ demos/smoke/ShellAndroid.cpp
@@ -115,13 +115,15 @@ std::vector<std::string> ShellAndroid::get_args(android_app &app)
 
 ShellAndroid::ShellAndroid(android_app &app, Game &game) : Shell(game), app_(app)
 {
-    instance_layers_.push_back("VK_LAYER_GOOGLE_threading");
-    instance_layers_.push_back("VK_LAYER_LUNARG_parameter_validation");
-    instance_layers_.push_back("VK_LAYER_LUNARG_object_tracker");
-    instance_layers_.push_back("VK_LAYER_LUNARG_image");
-    instance_layers_.push_back("VK_LAYER_LUNARG_core_validation");
-    instance_layers_.push_back("VK_LAYER_LUNARG_swapchain");
-    instance_layers_.push_back("VK_LAYER_GOOGLE_unique_objects");
+    if (game.settings().validate) {
+        instance_layers_.push_back("VK_LAYER_GOOGLE_threading");
+        instance_layers_.push_back("VK_LAYER_LUNARG_parameter_validation");
+        instance_layers_.push_back("VK_LAYER_LUNARG_object_tracker");
+        instance_layers_.push_back("VK_LAYER_LUNARG_image");
+        instance_layers_.push_back("VK_LAYER_LUNARG_core_validation");
+        instance_layers_.push_back("VK_LAYER_LUNARG_swapchain");
+        instance_layers_.push_back("VK_LAYER_GOOGLE_unique_objects");
+    }
 
     instance_extensions_.push_back(VK_KHR_ANDROID_SURFACE_EXTENSION_NAME);
 
