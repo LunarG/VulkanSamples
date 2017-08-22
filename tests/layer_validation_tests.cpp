5e0f7c895ae7f7b02b3644c7b0962bc1ba7efb42
Tobin Ehlis <tobine@google.com>
2017-08-22 15:50:30 -0600

    tests:clang-format fix-up

--- tests/layer_validation_tests.cpp
+++ tests/layer_validation_tests.cpp
@@ -8227,10 +8227,11 @@ TEST_F(VkLayerTest, DescriptorSetCompatibility) {
                             NULL);
     // TODO: Removed this perf warning and planning to update to only note disturbed descriptor when used unbound (See #2022)
     // 1. Disturb bound set0 by re-binding set1 w/ updated pipelineLayout
-//    m_errorMonitor->SetDesiredFailureMsg(VK_DEBUG_REPORT_PERFORMANCE_WARNING_BIT_EXT, " previously bound as set #0 was disturbed ");
-//    vkCmdBindDescriptorSets(m_commandBuffer->handle(), VK_PIPELINE_BIND_POINT_GRAPHICS, pipe_layout_bad_set0, 1, 1,
-//                            &descriptorSet[1], 0, NULL);
-//    m_errorMonitor->VerifyFound();
+    //    m_errorMonitor->SetDesiredFailureMsg(VK_DEBUG_REPORT_PERFORMANCE_WARNING_BIT_EXT, " previously bound as set #0 was
+    //    disturbed "); vkCmdBindDescriptorSets(m_commandBuffer->handle(), VK_PIPELINE_BIND_POINT_GRAPHICS, pipe_layout_bad_set0, 1,
+    //    1,
+    //                            &descriptorSet[1], 0, NULL);
+    //    m_errorMonitor->VerifyFound();
 
     vkCmdBindDescriptorSets(m_commandBuffer->handle(), VK_PIPELINE_BIND_POINT_GRAPHICS, pipeline_layout, 0, 2, &descriptorSet[0], 0,
                             NULL);
