46141d3279d9070d9af12f42991d7a249cb9e32d
Andreas Weis <der_ghulbus@ghulbus-inc.de>
2017-11-03 09:43:38 +0100

    scripts: python is no longer invoked via absolute path

--- update_external_sources.bat
+++ update_external_sources.bat
@@ -300,7 +300,7 @@ REM // ======== Functions ======== //
    cd %GLSLANG_DIR%
    git clone %GLSLANG_GITURL% .
    git checkout %GLSLANG_REVISION%
-   C:\Python27\python.exe .\update_glslang_sources.py
+   python.exe .\update_glslang_sources.py
    if not exist %GLSLANG_DIR%\SPIRV (
       echo glslang source download failed!
       set errorCode=1
@@ -313,7 +313,7 @@ goto:eof
    cd %GLSLANG_DIR%
    git fetch --all
    git checkout %GLSLANG_REVISION%
-   C:\Python27\python.exe .\update_glslang_sources.py
+   python.exe .\update_glslang_sources.py
 goto:eof
 
 :create_spirv-tools
