760e8dda2514006228cef62a4f3536d29564707d
Mark Lobodzinski <mark@lunarg.com>
2017-11-16 13:23:05 -0700

    build: Avoid redundant spirv-tools/hdrs builds
    Changed update_external_sources scripts to use spirv-tools and
    spirv-headers already present in the glslang repo instead of
    fetching and building separate versions.
    
    Change-Id: Iff9666f1939d814d0888b2da9b9e7450af697722

--- update_external_sources.bat
+++ update_external_sources.bat
@@ -1,5 +1,5 @@
 @echo off
-REM Update source for glslang, spirv-tools
+REM Update source for glslang
 
 REM Determine the appropriate CMake strings for the current version of Visual Studio
 echo Determining VS version
@@ -16,13 +16,11 @@ set BUILD_DIR=%~dp0
 set BASE_DIR="%BUILD_DIR%external"
 set REVISION_DIR="%BUILD_DIR%external_revisions"
 set GLSLANG_DIR=%BASE_DIR%\glslang
-set SPIRV_TOOLS_DIR=%BASE_DIR%\spirv-tools
 
 REM // ======== Parameter parsing ======== //
 
    set arg-use-implicit-component-list=1
    set arg-do-glslang=0
-   set arg-do-spirv-tools=0
    set arg-no-sync=0
    set arg-no-build=0
 
@@ -46,31 +44,6 @@ REM // ======== Parameter parsing ======== //
          goto:parameterLoop
       )
 
-      if "%1" == "--spirv-tools" (
-         set arg-do-spirv-tools=1
-         set arg-use-implicit-component-list=0
-         echo Building spirv-tools ^(%1^)
-         shift
-         goto:parameterLoop
-      )
-
-      if "%1" == "-s" (
-         set arg-do-spirv-tools=1
-         set arg-use-implicit-component-list=0
-         echo Building spirv-tools ^(%1^)
-         shift
-         goto:parameterLoop
-      )
-
-      if "%1" == "--all" (
-         set arg-do-glslang=1
-         set arg-do-spirv-tools=1
-         set arg-use-implicit-component-list=0
-         echo Building glslang, spirv-tools ^(%1^)
-         shift
-         goto:parameterLoop
-      )
-
       if "%1" == "--no-sync" (
          set arg-no-sync=1
          echo Skipping sync ^(%1^)
@@ -85,6 +58,27 @@ REM // ======== Parameter parsing ======== //
          goto:parameterLoop
       )
 
+      if "%1" == "--spirv-tools" (
+         echo --spirv-tools argument has been deprecated and is no longer necessary
+         shift
+         goto:parameterLoop
+      )
+
+      if "%1" == "-s" (
+         echo --s argument has been deprecated and is no longer necessary
+         shift
+         goto:parameterLoop
+      )
+
+      if "%1" == "--all" (
+         echo --all argument has been deprecated and is no longer necessary
+         set arg-do-glslang=1
+         set arg-use-implicit-component-list=0
+         echo Building glslang ^(%1^)
+         shift
+         goto:parameterLoop
+      )
+
       echo.
       echo Unrecognized option "%1"
       echo.
@@ -92,7 +86,6 @@ REM // ======== Parameter parsing ======== //
       echo.
       echo   Available options:
       echo     -g ^| --glslang      enable glslang component
-      echo     -s ^| --spirv-tools  enable spirv-tools component
       echo     --all               enable all components
       echo     --no-sync           skip sync from git
       echo     --no-build          skip build
@@ -109,15 +102,12 @@ REM // ======== Parameter parsing ======== //
    :parameterContinue
 
    if %arg-use-implicit-component-list% equ 1 (
-      echo Building glslang, spirv-tools
+      echo Building glslang
       set arg-do-glslang=1
-      set arg-do-spirv-tools=1
    )
 
    set sync-glslang=0
-   set sync-spirv-tools=0
    set build-glslang=0
-   set build-spirv-tools=0
    set check-glslang-build-dependencies=0
 
    if %arg-do-glslang% equ 1 (
@@ -130,24 +120,11 @@ REM // ======== Parameter parsing ======== //
       )
    )
 
-   if %arg-do-spirv-tools% equ 1 (
-      if %arg-no-sync% equ 0 (
-         set sync-spirv-tools=1
-      )
-      if %arg-no-build% equ 0 (
-         REM glslang has the same dependencies as spirv-tools
-         set check-glslang-build-dependencies=1
-         set build-spirv-tools=1
-      )
-   )
-
    REM this is a debugging aid that can be enabled while debugging command-line parsing
    if 0 equ 1 (
       set arg
       set sync-glslang
-      set sync-spirv-tools
       set build-glslang
-      set build-spirv-tools
       set check-glslang-build-dependencies
       goto:error
    )
@@ -202,50 +179,14 @@ if not exist %REVISION_DIR%\glslang_revision (
    goto:error
 )
 
-if not exist %REVISION_DIR%\spirv-tools_giturl (
-   echo.
-   echo Missing spirv-tools_giturl file!  Place it in %REVISION_DIR% with git repo URL in it.
-   set errorCode=1
-   goto:error
-)
-
-if not exist %REVISION_DIR%\spirv-tools_revision (
-   echo.
-   echo Missing spirv-tools_revision file!  Place it in %REVISION_DIR% with target version in it.
-   set errorCode=1
-   goto:error
-)
-
-if not exist %REVISION_DIR%\spirv-headers_giturl (
-   echo.
-   echo Missing spirv-headers_giturl file!  Place it in %REVISION_DIR% with git repo URL in it.
-   set errorCode=1
-   goto:error
-)
-
-if not exist %REVISION_DIR%\spirv-headers_revision (
-   echo.
-   echo Missing spirv-headers_revision file!  Place it in %REVISION_DIR% with target version in it.
-   set errorCode=1
-   goto:error
-)
-
 set /p GLSLANG_GITURL= < %REVISION_DIR%\glslang_giturl
 set /p GLSLANG_REVISION= < %REVISION_DIR%\glslang_revision
-set /p SPIRV_TOOLS_GITURL= < %REVISION_DIR%\spirv-tools_giturl
-set /p SPIRV_TOOLS_REVISION= < %REVISION_DIR%\spirv-tools_revision
-set /p SPIRV_HEADERS_GITURL= < %REVISION_DIR%\spirv-headers_giturl
-set /p SPIRV_HEADERS_REVISION= < %REVISION_DIR%\spirv-headers_revision
 
 echo GLSLANG_GITURL=%GLSLANG_GITURL%
 echo GLSLANG_REVISION=%GLSLANG_REVISION%
-echo SPIRV_TOOLS_GITURL=%SPIRV_TOOLS_GITURL%
-echo SPIRV_TOOLS_REVISION=%SPIRV_TOOLS_REVISION%
-echo SPIRV_HEADERS_GITURL=%SPIRV_HEADERS_GITURL%
-echo SPIRV_HEADERS_REVISION=%SPIRV_HEADERS_REVISION%
 
 
-echo Creating and/or updating glslang, spirv-tools in %BASE_DIR%
+echo Creating and/or updating glslang in %BASE_DIR%
 
 if %sync-glslang% equ 1 (
    if not exist %GLSLANG_DIR% (
@@ -256,23 +197,8 @@ if %sync-glslang% equ 1 (
    if %errorCode% neq 0 (goto:error)
 )
 
-if %sync-spirv-tools% equ 1 (
-   if %errorlevel% neq 0 (goto:error)
-   if not exist %SPIRV_TOOLS_DIR% (
-      call:create_spirv-tools
-   )
-   if %errorCode% neq 0 (goto:error)
-   call:update_spirv-tools
-   if %errorCode% neq 0 (goto:error)
-)
-
 if %build-glslang% equ 1 (
    call:build_glslang
-   if %errorCode% neq 0 (goto:error)
-)
-
-if %build-spirv-tools% equ 1 (
-   call:build_spirv-tools
    if %errorCode% neq 0 (goto:error)
 )
 
@@ -302,8 +228,8 @@ REM // ======== Functions ======== //
    git checkout %GLSLANG_REVISION%
    python.exe .\update_glslang_sources.py
    if not exist %GLSLANG_DIR%\SPIRV (
-      echo glslang source download failed!
-      set errorCode=1
+       echo glslang source download failed!
+       set errorCode=1
    )
 goto:eof
 
@@ -316,62 +242,30 @@ goto:eof
    python.exe .\update_glslang_sources.py
 goto:eof
 
-:create_spirv-tools
-   echo.
-   echo Creating local spirv-tools repository %SPIRV_TOOLS_DIR%)
-   mkdir %SPIRV_TOOLS_DIR%
-   cd %SPIRV_TOOLS_DIR%
-   git clone %SPIRV_TOOLS_GITURL% .
-   git checkout %SPIRV_TOOLS_REVISION%
-   if not exist %SPIRV_TOOLS_DIR%\source (
-      echo spirv-tools source download failed!
-      set errorCode=1
-   )
-   mkdir %SPIRV_TOOLS_DIR%\external
-   mkdir %SPIRV_TOOLS_DIR%\external\spirv-headers
-   cd %SPIRV_TOOLS_DIR%\external\spirv-headers
-   git clone %SPIRV_HEADERS_GITURL% .
-   git checkout %SPIRV_HEADERS_REVISION%
-   if not exist %SPIRV_TOOLS_DIR%\external\spirv-headers\README.md (
-      echo spirv-headers download failed!
-      set errorCode=1
-   )
-goto:eof
-
-:update_spirv-tools
-   echo.
-   echo Updating %SPIRV_TOOLS_DIR%
-   cd %SPIRV_TOOLS_DIR%
-   git fetch --all
-   git checkout %SPIRV_TOOLS_REVISION%
-   cd %SPIRV_TOOLS_DIR%\external\spirv-headers
-   git fetch --all
-   git checkout %SPIRV_HEADERS_REVISION%
-goto:eof
-
 :build_glslang
    echo.
    echo Building %GLSLANG_DIR%
    cd  %GLSLANG_DIR%
-   
+
    if not exist build32 (
        mkdir build32
    )
    if not exist build (
-      mkdir build
+       mkdir build
    )
-   
+
    echo Making 32-bit glslang
    echo *************************
+
    set GLSLANG_BUILD_DIR=%GLSLANG_DIR%\build32
    cd %GLSLANG_BUILD_DIR%
 
    echo Generating 32-bit Glslang CMake files for Visual Studio %VS_VERSION% -DCMAKE_INSTALL_PREFIX=install ..
    cmake -G "Visual Studio %VS_VERSION%" -DCMAKE_INSTALL_PREFIX=install ..
-   
+
    echo Building 32-bit Glslang: MSBuild INSTALL.vcxproj /p:Platform=x86 /p:Configuration=Debug
    msbuild INSTALL.vcxproj /p:Platform=x86 /p:Configuration=Debug /verbosity:quiet
-   
+
    REM Check for existence of one lib, even though we should check for all results
    if not exist %GLSLANG_BUILD_DIR%\glslang\Debug\glslangd.lib (
       echo.
@@ -380,16 +274,16 @@ goto:eof
    )
    echo Building Glslang: MSBuild INSTALL.vcxproj /p:Platform=x86 /p:Configuration=Release
    msbuild INSTALL.vcxproj /p:Platform=x86 /p:Configuration=Release /verbosity:quiet
-   
+
    REM Check for existence of one lib, even though we should check for all results
    if not exist %GLSLANG_BUILD_DIR%\glslang\Release\glslang.lib (
       echo.
       echo glslang 32-bit Release build failed!
       set errorCode=1
    )
-   
+
    cd ..
- 
+
    echo Making 64-bit glslang
    echo *************************
    set GLSLANG_BUILD_DIR=%GLSLANG_DIR%\build
@@ -397,96 +291,24 @@ goto:eof
 
    echo Generating 64-bit Glslang CMake files for Visual Studio %VS_VERSION% -DCMAKE_INSTALL_PREFIX=install ..
    cmake -G "Visual Studio %VS_VERSION% Win64" -DCMAKE_INSTALL_PREFIX=install ..
-   
+
    echo Building 64-bit Glslang: MSBuild INSTALL.vcxproj /p:Platform=x64 /p:Configuration=Debug
    msbuild INSTALL.vcxproj /p:Platform=x64 /p:Configuration=Debug /verbosity:quiet
-   
+
    REM Check for existence of one lib, even though we should check for all results
    if not exist %GLSLANG_BUILD_DIR%\glslang\Debug\glslangd.lib (
-      echo.
-      echo glslang 64-bit Debug build failed!
-      set errorCode=1
+       echo.
+       echo glslang 64-bit Debug build failed!
+       set errorCode=1
    )
+
    echo Building Glslang: MSBuild INSTALL.vcxproj /p:Platform=x64 /p:Configuration=Release
    msbuild INSTALL.vcxproj /p:Platform=x64 /p:Configuration=Release /verbosity:quiet
-   
+
    REM Check for existence of one lib, even though we should check for all results
    if not exist %GLSLANG_BUILD_DIR%\glslang\Release\glslang.lib (
-      echo.
-      echo glslang 64-bit Release build failed!
-      set errorCode=1
-   )
-goto:eof
-
-:build_spirv-tools
-   echo.
-   echo Building %SPIRV_TOOLS_DIR%
-   cd  %SPIRV_TOOLS_DIR%
-
-   REM Cleanup any old directories lying around.
-   if not exist build32 (
-       mkdir build32
-   )
-   if not exist build (
-      mkdir build
-   )
-
-   echo Making 32-bit spirv-tools
-   echo *************************
-   set SPIRV_TOOLS_BUILD_DIR=%SPIRV_TOOLS_DIR%\build32
-
-   cd %SPIRV_TOOLS_BUILD_DIR%
-   
-   echo Generating 32-bit spirv-tools CMake files for Visual Studio %VS_VERSION% ..
-   cmake -G "Visual Studio %VS_VERSION%" ..
-   
-   echo Building 32-bit spirv-tools: MSBuild ALL_BUILD.vcxproj /p:Platform=x86 /p:Configuration=Debug
-   msbuild ALL_BUILD.vcxproj /p:Platform=x86 /p:Configuration=Debug /verbosity:quiet
-   
-   REM Check for existence of one lib, even though we should check for all results
-   if not exist %SPIRV_TOOLS_BUILD_DIR%\source\Debug\SPIRV-Tools.lib (
-      echo.
-      echo spirv-tools 32-bit Debug build failed!
-      set errorCode=1
-   )
-   
-   echo Building 32-bit spirv-tools: MSBuild ALL_BUILD.vcxproj /p:Platform=x86 /p:Configuration=Release
-   msbuild ALL_BUILD.vcxproj /p:Platform=x86 /p:Configuration=Release /verbosity:quiet
-
-   REM Check for existence of one lib, even though we should check for all results
-   if not exist %SPIRV_TOOLS_BUILD_DIR%\source\Release\SPIRV-Tools.lib (
-      echo.
-      echo spirv-tools 32-bit Release build failed!
-      set errorCode=1
-   )
-   
-   cd ..
- 
-   echo Making 64-bit spirv-tools  
-   echo *************************
-   set SPIRV_TOOLS_BUILD_DIR=%SPIRV_TOOLS_DIR%\build
-   cd %SPIRV_TOOLS_BUILD_DIR%
-
-   echo Generating 64-bit spirv-tools CMake files for Visual Studio %VS_VERSION% ..
-   cmake -G "Visual Studio %VS_VERSION% Win64" ..
-   
-   echo Building 64-bit spirv-tools: MSBuild ALL_BUILD.vcxproj /p:Platform=x64 /p:Configuration=Debug
-   msbuild ALL_BUILD.vcxproj /p:Platform=x64 /p:Configuration=Debug /verbosity:quiet
-   
-   REM Check for existence of one lib, even though we should check for all results
-   if not exist %SPIRV_TOOLS_BUILD_DIR%\source\Debug\SPIRV-Tools.lib (
-      echo.
-      echo spirv-tools 64-bit Debug build failed!
-      set errorCode=1
-   )
-   
-   echo Building 64-bit spirv-tools: MSBuild ALL_BUILD.vcxproj /p:Platform=x64 /p:Configuration=Release
-   msbuild ALL_BUILD.vcxproj /p:Platform=x64 /p:Configuration=Release /verbosity:quiet
-
-   REM Check for existence of one lib, even though we should check for all results
-   if not exist %SPIRV_TOOLS_BUILD_DIR%\source\Release\SPIRV-Tools.lib (
-      echo.
-      echo spirv-tools 64-bit Release build failed!
-      set errorCode=1
+       echo.
+       echo glslang 64-bit Release build failed!
+       set errorCode=1
    )
 goto:eof
--- update_external_sources.sh
+++ update_external_sources.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Update source for glslang, spirv-tools
+# Update source for glslang
 
 set -e
 
@@ -17,17 +17,9 @@ REVISION_DIR="$CURRENT_DIR/external_revisions"
 
 GLSLANG_GITURL=$(cat "${REVISION_DIR}/glslang_giturl")
 GLSLANG_REVISION=$(cat "${REVISION_DIR}/glslang_revision")
-SPIRV_TOOLS_GITURL=$(cat "${REVISION_DIR}/spirv-tools_giturl")
-SPIRV_TOOLS_REVISION=$(cat "${REVISION_DIR}/spirv-tools_revision")
-SPIRV_HEADERS_GITURL=$(cat "${REVISION_DIR}/spirv-headers_giturl")
-SPIRV_HEADERS_REVISION=$(cat "${REVISION_DIR}/spirv-headers_revision")
 
 echo "GLSLANG_GITURL=${GLSLANG_GITURL}"
 echo "GLSLANG_REVISION=${GLSLANG_REVISION}"
-echo "SPIRV_TOOLS_GITURL=${SPIRV_TOOLS_GITURL}"
-echo "SPIRV_TOOLS_REVISION=${SPIRV_TOOLS_REVISION}"
-echo "SPIRV_HEADERS_GITURL=${SPIRV_HEADERS_GITURL}"
-echo "SPIRV_HEADERS_REVISION=${SPIRV_HEADERS_REVISION}"
 
 BUILDDIR=${CURRENT_DIR}
 BASEDIR="$BUILDDIR/external"
@@ -50,35 +42,6 @@ function update_glslang () {
    ./update_glslang_sources.py
 }
 
-function create_spirv-tools () {
-   rm -rf "${BASEDIR}"/spirv-tools
-   echo "Creating local spirv-tools repository (${BASEDIR}/spirv-tools)."
-   mkdir -p "${BASEDIR}"/spirv-tools
-   cd "${BASEDIR}"/spirv-tools
-   git clone ${SPIRV_TOOLS_GITURL} .
-   git checkout ${SPIRV_TOOLS_REVISION}
-   mkdir -p "${BASEDIR}"/spirv-tools/external/spirv-headers
-   cd "${BASEDIR}"/spirv-tools/external/spirv-headers
-   git clone ${SPIRV_HEADERS_GITURL} .
-   git checkout ${SPIRV_HEADERS_REVISION}
-}
-
-function update_spirv-tools () {
-   echo "Updating ${BASEDIR}/spirv-tools"
-   cd "${BASEDIR}"/spirv-tools
-   git fetch --all
-   git checkout ${SPIRV_TOOLS_REVISION}
-   if [ ! -d "${BASEDIR}/spirv-tools/external/spirv-headers" -o ! -d "${BASEDIR}/spirv-tools/external/spirv-headers/.git" ]; then
-      mkdir -p "${BASEDIR}"/spirv-tools/external/spirv-headers
-      cd "${BASEDIR}"/spirv-tools/external/spirv-headers
-      git clone ${SPIRV_HEADERS_GITURL} .
-   else
-      cd "${BASEDIR}"/spirv-tools/external/spirv-headers
-      git fetch --all
-   fi
-   git checkout ${SPIRV_HEADERS_REVISION}
-}
-
 function build_glslang () {
    echo "Building ${BASEDIR}/glslang"
    cd "${BASEDIR}"/glslang
@@ -89,17 +52,7 @@ function build_glslang () {
    make install
 }
 
-function build_spirv-tools () {
-   echo "Building ${BASEDIR}/spirv-tools"
-   cd "${BASEDIR}"/spirv-tools
-   mkdir -p build
-   cd build
-   cmake -D CMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=install ..
-   make -j $CORE_COUNT
-}
-
 INCLUDE_GLSLANG=false
-INCLUDE_SPIRV_TOOLS=false
 NO_SYNC=false
 NO_BUILD=false
 USE_IMPLICIT_COMPONENT_LIST=true
@@ -118,9 +71,7 @@ do
       ;;
       # options to specify build of spirv-tools components
       -s|--spirv-tools)
-      INCLUDE_SPIRV_TOOLS=true
-      USE_IMPLICIT_COMPONENT_LIST=false
-      echo "Building spirv-tools ($option)"
+      echo "($option) is deprecated and is no longer necessary"
       ;;
       # option to specify skipping sync from git
       --no-sync)
@@ -137,7 +88,6 @@ do
       echo "Usage: update_external_sources.sh [options]"
       echo "  Available options:"
       echo "    -g | --glslang      # enable glslang component"
-      echo "    -s | --spirv-tools  # enable spirv-tools component"
       echo "    --no-sync           # skip sync from git"
       echo "    --no-build          # skip build"
       echo "  If any component enables are provided, only those components are enabled."
@@ -151,9 +101,8 @@ do
 done
 
 if [ ${USE_IMPLICIT_COMPONENT_LIST} == "true" ]; then
-  echo "Building glslang, spirv-tools"
+  echo "Building glslang"
   INCLUDE_GLSLANG=true
-  INCLUDE_SPIRV_TOOLS=true
 fi
 
 if [ ${INCLUDE_GLSLANG} == "true" ]; then
@@ -165,18 +114,5 @@ if [ ${INCLUDE_GLSLANG} == "true" ]; then
   fi
   if [ ${NO_BUILD} == "false" ]; then
     build_glslang
-  fi
-fi
-
-
-if [ ${INCLUDE_SPIRV_TOOLS} == "true" ]; then
-  if [ ${NO_SYNC} == "false" ]; then
-    if [ ! -d "${BASEDIR}/spirv-tools" -o ! -d "${BASEDIR}/spirv-tools/.git" ]; then
-       create_spirv-tools
-    fi
-    update_spirv-tools
-  fi
-  if [ ${NO_BUILD} == "false" ]; then
-    build_spirv-tools
   fi
 fi
